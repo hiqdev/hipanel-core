111a1615f5987b8a42eb024ab4f2040814f61fe2
Tafid <andrus2005@ukr.net>
2015-01-12 00:17:43 +0200

    Add new API Lang class

--- common/components/Lang.php
+++ common/components/Lang.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tofid
- * Date: 11.01.15
- * Time: 23:05
- */
+namespace common\components;
+
+use Yii;
+
+class Lang
+{
+    /**
+     * AHNAMES Lng to Yii2 i18n
+     * @param null $txt
+     * @param string $path
+     *
+     * @return string
+     */
+    public static function l($txt=null, $path='app') {
+        if ($txt!==null) {
+            return $t=Yii::t($path, str_ireplace(['{lang:','}'], '', $txt));
+        }
+    }
+}
