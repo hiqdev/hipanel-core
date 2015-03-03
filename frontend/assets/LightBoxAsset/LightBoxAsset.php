10d7a8e95521f5c05ed4c5c35e32fde875871d1a
Andrey Klochok <andreyklochok@gmail.com>
2015-03-03 12:43:34 +0200

    Add LoghtBox Asset

---
+++ frontend/assets/LightBoxAsset/LightBoxAsset.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tofid
+ * Date: 27.02.15
+ * Time: 16:51
+ */
---
+++ frontend/widgets/FileRender.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tofid
+ * Date: 26.02.15
+ * Time: 17:37
+ */
