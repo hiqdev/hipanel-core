bd158c11fcde0cae6f04a54545a1c21ffbdc0a91
Andrey Klochok <andreyklochok@gmail.com>
2015-03-03 12:45:31 +0200

    Add LightBoxAsset class

--- frontend/assets/LightBoxAsset/LightBoxAsset.php
+++ frontend/assets/LightBoxAsset/LightBoxAsset.php
@@ -5,3 +5,24 @@
  * Date: 27.02.15
  * Time: 16:51
  */
+
+namespace frontend\assets\LightBoxAsset;
+
+use kartik\base\AssetBundle;
+
+class LightBoxAsset extends AssetBundle
+{
+    public $sourcePath = '@app/assets/LightBoxAsset/asset';
+
+    public $css = [
+        'css/lightbox.css',
+    ];
+
+    public $js = [
+        'js/lightbox.min.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
