c4e9a43b7964aaf3437d75ba488efa16246e1c4b
Andrey Klochok <andreyklochok@gmail.com>
2015-03-10 19:07:15 +0200

    Fix! Pjax widget misplaced bracket

---
+++ frontend/views/skin/_form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tofid
+ * Date: 10.03.15
+ * Time: 13:55
+ */
---
+++ frontend/views/skin/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tofid
+ * Date: 06.03.15
+ * Time: 18:56
+ */
