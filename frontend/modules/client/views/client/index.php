db91415fca33ac9d38c69982093788eb16de951d
Andrey Klochok <andreyklochok@gmail.com>
2015-04-10 15:51:30 +0300

    Init custom ActionColumn with DropDownButtons

--- frontend/modules/client/views/client/index.php
+++ frontend/modules/client/views/client/index.php
@@ -1,83 +1,84 @@
 <?php
 
+use frontend\components\grid\BoxedGridView;
 use frontend\components\grid\CheckboxColumn;
 use frontend\components\grid\CurrentColumn;
 use frontend\components\grid\EditableColumn;
+use frontend\components\widgets\Box;
 use frontend\modules\client\grid\ResellerColumn;
-use frontend\components\grid\SwitchColumn;
 use frontend\modules\domain\widgets\State;
-
 use yii\bootstrap\ButtonGroup;
-use frontend\components\grid\GridView;
+use yii\helpers\ArrayHelper;
 use yii\web\View;
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\jui\DatePicker;
-use yii\web\JsExpression;
-use yii\widgets\Pjax;
 use yii\bootstrap\Modal;
-use frontend\modules\thread\widgets\Label;
-use frontend\components\Re;
+
+//use frontend\components\grid\GridView;
+//use yii\web\JsExpression;
+//use yii\widgets\Pjax;
+//use frontend\components\grid\SwitchColumn;
+//use frontend\modules\thread\widgets\Label;
+//use frontend\components\Re;
 
 $this->title = 'Client';
 $this->params['breadcrumbs'][] = $this->title;
 
 $widgetButtonConfig = [
-    'buttons'   => [
+    'buttons' => [
         [
-            'label'     => Yii::t('app','Tariff'),
-            'options'   => [
-                'class'     => 'btn-xs' . ($add['tpl'] == '_tariff' ? ' active' : ''),
+            'label' => Yii::t('app', 'Tariff'),
+            'options' => [
+                'class' => 'btn-default' . ($add['tpl'] == '_tariff' ? ' active' : ''),
                 'data-view' => '_tariff'
             ],
         ],
         [
-            'label'     => Yii::t('app','Card'),
-            'options'   => [
-                'class'     => 'btn-xs' . ($add['tpl'] == '_card' ? ' active' : ''),
+            'label' => Yii::t('app', 'Card'),
+            'options' => [
+                'class' => 'btn-default' . ($add['tpl'] == '_card' ? ' active' : ''),
                 'data-view' => '_card',
             ],
         ],
     ],
-    'options'   => ['class'=>'change-view-button']
+    'options' => ['class' => 'change-view-button']
 ];
 ?>
-<div class="row">
-    <div class="col-md-1 col-md-offset-11" style="margin: 10px">
-        <?= ButtonGroup::widget($widgetButtonConfig); ?>
-    </div>
-</div>
+<?php $box = Box::begin(); ?>
+<?= ButtonGroup::widget($widgetButtonConfig); ?>
+<?php $box::end(); ?>
 
 <?php
 $widgetIndexConfig = [
-    'dataProvider'  => $dataProvider,
-    'filterModel'   => $searchModel,
-    'columns'       => [
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
         [
-            'class'         => CheckboxColumn::className(),
+            'class' => CheckboxColumn::className(),
         ],
         [
-            'class'         => CurrentColumn::className(),
-            'uses'          => [
-                'rename'       => [
-                    'text'         => 'login',
+            'class' => CurrentColumn::className(),
+            'uses' => [
+                'rename' => [
+                    'text' => 'login',
                 ],
-                'return'        => ['id'],
-                'term'          => 'client_like:term',
-                'wrapper'       => 'results',
+                'return' => ['id'],
+                'term' => 'client_like:term',
+                'wrapper' => 'results',
             ],
-            'attribute'     => 'login',
-            'label'         => Yii::t('app', 'Client'),
-            'value'         => function ($model) {
-                return Html::a($model->login, ['view', 'id' => $model->id]); 
+            'attribute' => 'login',
+            'label' => Yii::t('app', 'Client'),
+            'value' => function ($model) {
+                return Html::a($model->login, ['view', 'id' => $model->id]);
             },
         ],
         [
-            'class'         => ResellerColumn::className(),
+            'class' => ResellerColumn::className(),
         ],
         [
             'attribute' => 'type',
-            'label'     => Yii::t('app','Type'),
+            'label' => Yii::t('app', 'Type'),
             'filter' => Html::activeDropDownList($searchModel, 'type', \frontend\models\Ref::getList('type,client', true), [
                 'class' => 'form-control',
                 'prompt' => Yii::t('app', '--'),
@@ -85,7 +86,7 @@ $widgetIndexConfig = [
         ],
         [
             'attribute' => 'state',
-            'label'     => Yii::t('app','State'),
+            'label' => Yii::t('app', 'State'),
             'filter' => Html::activeDropDownList($searchModel, 'state', \frontend\models\Ref::getList('state,client', true), [
                 'class' => 'form-control',
                 'prompt' => Yii::t('app', '--'),
@@ -96,118 +97,127 @@ $widgetIndexConfig = [
 ];
 switch ($add['tpl']) {
     case '_card':
-        $widgetIndexConfig['columns'] = \yii\helpers\ArrayHelper::merge($widgetIndexConfig['columns'], [
+        $widgetIndexConfig['columns'] = ArrayHelper::merge($widgetIndexConfig['columns'], [
             [
                 'attribute' => 'name',
-                'label'     => Yii::t('app','Name'),
+                'label' => Yii::t('app', 'Name'),
             ],
             [
-                'label'     => Yii::t('app','Balance'),
-                'format'    => 'html',
-                'value'     => function ($data) {
-                    return Yii::t('app','Balance') . ": " . HTML::tag('span', $data->balance, $data->balance < 0 ? 'color="red"' : '');
+                'label' => Yii::t('app', 'Balance'),
+                'format' => 'html',
+                'value' => function ($data) {
+                    return Yii::t('app', 'Balance') . ": " . HTML::tag('span', $data->balance, $data->balance < 0 ? 'color="red"' : '');
                 },
             ],
             [
-                'class'                 => EditableColumn::className(),
-                'attribute'             => 'credit',
-                'filter'                => false,
-                'popover'               => Yii::t('app','Set credit'),
-                'action'                => ['set-credit']
+                'class' => EditableColumn::className(),
+                'attribute' => 'credit',
+                'filter' => false,
+                'popover' => Yii::t('app', 'Set credit'),
+                'action' => ['set-credit']
             ],
             [
-                'label'     => Yii::t('app','Credit'),
+                'label' => Yii::t('app', 'Credit'),
             ],
         ]);
         break;
     case '_tariff':
-        $widgetIndexConfig['columns'] = \yii\helpers\ArrayHelper::merge($widgetIndexConfig['columns'], [
+        $widgetIndexConfig['columns'] = ArrayHelper::merge($widgetIndexConfig['columns'], [
             [
-                'attribute'     =>'tariff_name',
-                'format'        => 'html',
-                'filter'        => Html::dropDownList(
-                    'tariff_name',
-                    ['1'],
-                    [1=>1,2=>2],
-                    [
-                        'class'     => 'form-control',
-                        'prompt'    => Yii::t('app', 'BACKEND_PROMPT_STATUS'),
-                    ]
-                ),
+                'attribute' => 'tariff_name',
+                'format' => 'html',
+                'filter' => Html::dropDownList('tariff_name', ['1'], [1 => 1, 2 => 2], [
+                    'class' => 'form-control',
+                    'prompt' => Yii::t('app', 'BACKEND_PROMPT_STATUS'),
+                ]),
             ],
         ]);
         break;
     default:
-        $widgetIndexConfig['columns'] = \yii\helpers\ArrayHelper::merge($widgetIndexConfig['columns'], [
+        $widgetIndexConfig['columns'] = ArrayHelper::merge($widgetIndexConfig['columns'], [
             [
                 'attribute' => 'name',
-                'label'     => Yii::t('app','Name'),
+                'label' => Yii::t('app', 'Name'),
             ],
             [
-                'label'     => Yii::t('app','Balance'),
-                'format'    => 'html',
-                'value'     => function ($data) {
-                    return Yii::t('app','Balance') . ": " . HTML::tag('span', $data->balance, $data->balance < 0 ? 'color="red"' : '');
+                'label' => Yii::t('app', 'Balance'),
+                'format' => 'html',
+                'value' => function ($data) {
+                    return Yii::t('app', 'Balance') . ": " . HTML::tag('span', $data->balance, $data->balance < 0 ? 'color="red"' : '');
                 },
             ],
             [
-                'class'                 => EditableColumn::className(),
-                'attribute'             => 'credit',
-                'filter'                => false,
-                'popover'               => Yii::t('app','Set credit'),
-                'action'                => ['set-credit']
+                'class' => EditableColumn::className(),
+                'attribute' => 'credit',
+                'filter' => false,
+                'popover' => Yii::t('app', 'Set credit'),
+                'action' => ['set-credit']
             ],
             [
-                'label'     => Yii::t('app','Credit'),
+                'label' => Yii::t('app', 'Credit'),
             ],
         ]);
         break;
 
 }
 
-$widgetIndexConfig['columns'] = \yii\helpers\ArrayHelper::merge($widgetIndexConfig['columns'], [
+$widgetIndexConfig['columns'] = ArrayHelper::merge($widgetIndexConfig['columns'], [
     [
         'attribute' => 'create_time',
-        'label'     => Yii::t('app','Create date'),
-        'format'    => ['date', 'php:Y-m-d'],
-        'filter'    => DatePicker::widget([
-            'model'         => $searchModel,
-            'name'          => 'create_time',
-            'dateFormat'    => 'yyyy-MM-dd',
-            'attribute'     => 'create_time',
+        'label' => Yii::t('app', 'Create date'),
+        'format' => ['date', 'php:Y-m-d'],
+        'filter' => DatePicker::widget([
+            'model' => $searchModel,
+            'name' => 'create_time',
+            'dateFormat' => 'yyyy-MM-dd',
+            'attribute' => 'create_time',
         ]),
     ],
     [
-        'class'         => 'yii\grid\ActionColumn',
-        'template'      => '{view} {update} {block} {delete} {set-credit}',
-        'buttons'       => [
-            'view'          => function ($url, $model, $key) {
-                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view','id'=>$model['id']]);
+        //        'class'         => 'yii\grid\ActionColumn',
+        'class' => 'frontend\components\grid\ActionColumn',
+        'template' => '{view} {update} {block} {delete} {set-credit}',
+        'buttons' => [
+            //            'view'          => function ($url, $model, $key) {
+            //                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view','id'=>$model['id']]);
+            //            },
+            'block' => function ($url, $model, $key) {
+                if ($model->state == 'ok')
+                    return Html::a(Yii::t('app', 'Block'), [
+                        'enable-block',
+                        'id' => $model['id']
+                    ], [
+                        'class' => "client-block enable",
+                        'title' => Yii::t('app', 'Enable') . " " . Yii::t('app', 'block'),
+                        'value-id' => $model['id']
+                    ]);
+                elseif ($model->state == 'blocked')
+                    return Html::a(Yii::t('app', 'Block'), [
+                        'disable-block',
+                        'id' => $model['id']
+                    ], [
+                        'class' => "client-block disable",
+                        'title' => Yii::t('app', 'Disable') . " " . Yii::t('app', 'block'),
+                        'value-id' => $model['id']
+                    ]);
             },
-            'block'         => function ($url, $model, $key) {
-                if ($model->state == 'ok') return Html::a('<span class="glyphicon glyphicon-log-in"></span>', ['enable-block','id'=>$model['id']],
-                    [ 'class' => "client-block enable", 'title' => Yii::t('app', 'Enable') . " " . Yii::t('app', 'block'), 'value-id' => $model['id'] ]);
-                elseif ($model->state == 'blocked') return Html::a('<span class="glyphicon glyphicon-log-out"></span>', ['disable-block','id'=>$model['id']],
-                    [ 'class' => "client-block disable", 'title' => Yii::t('app', 'Disable') . " " . Yii::t('app', 'block'), 'value-id' => $model['id'] ]);
-            },
-            'delete'        => function ($url, $model, $key) {
-                if ($model->state != 'deleted') return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete','id'=>$model['id']],
-                    [ 'title' => Yii::t('app', 'Delete'), 'data-pjax' => 0, 'class' => 'delete-client', 'value-id' => $model['id'] ] );
-            },
-            'set-credit'    => function ($url, $model, $key) {
-                return Html::a('<span class="glyphicon glyphicon-usd"><span>', ['set-credit', 'id' => $model['id']],
-                    [ 'class' => 'set-credit', 'title' => Yii::t('app', 'Set credit'), 'value-id' => $model['id']] );
+            //            'delete'        => function ($url, $model, $key) {
+            //                if ($model->state != 'deleted') return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete','id'=>$model['id']],
+            //                    [ 'title' => Yii::t('app', 'Delete'), 'data-pjax' => 0, 'class' => 'delete-client', 'value-id' => $model['id'] ] );
+            //            },
+            'set-credit' => function ($url, $model, $key) {
+                return Html::a(Yii::t('app', 'Set credit'), [
+                    'set-credit',
+                    'id' => $model['id']
+                ], ['class' => 'set-credit', 'title' => Yii::t('app', 'Set credit'), 'value-id' => $model['id']]);
             }
         ],
     ],
 ]);
 
 ?>
-<div class="box box-primary">
-    <div class="box-body"
-        <?= GridView::widget($widgetIndexConfig); ?>
-    </div>
-</div>
+
+<?= BoxedGridView::widget($widgetIndexConfig); ?>
 
 <?php
 echo Html::beginForm([
@@ -215,13 +225,13 @@ echo Html::beginForm([
 ], "POST", ['data' => ['pjax' => 1], 'class' => 'inline', 'id' => 'set-credit-form']);
 
 Modal::begin([
-    'id'            => 'set-credit-form-modal',
-    'header'        => Html::tag('h4', Yii::t('app', 'Set credits for user')),
+    'id' => 'set-credit-form-modal',
+    'header' => Html::tag('h4', Yii::t('app', 'Set credits for user')),
     'headerOptions' => ['class' => 'label-warning'],
-    'footer'        => Html::button(Yii::t('app', 'Set credit'), [
-        'class'             => 'btn btn-warning',
+    'footer' => Html::button(Yii::t('app', 'Set credit'), [
+        'class' => 'btn btn-warning',
         'data-loading-text' => Yii::t('app', 'Setting...'),
-        'onClick'           => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
+        'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
     ]),
 ]); ?>
 <div id="set-credit-contents"></div>
@@ -235,16 +245,16 @@ echo Html::endForm();
 ], "GET", ['data' => ['pjax' => 1], 'class' => 'inline', 'id' => 'delete-client']);
 
 ?>
-<input type="hidden" class='delete-value' readonly='readonly' name="id" />
+<input type="hidden" class='delete-value' readonly='readonly' name="id"/>
 <?php
 Modal::begin([
-    'id'            => 'delete-client-form-modal',
-    'header'        => Html::tag('h4', Yii::t('app', 'Delete client')),
+    'id' => 'delete-client-form-modal',
+    'header' => Html::tag('h4', Yii::t('app', 'Delete client')),
     'headerOptions' => ['class' => 'label-warning'],
-    'footer'        => Html::button(Yii::t('app', 'Delete client'), [
-        'class'             => 'btn btn-warning',
+    'footer' => Html::button(Yii::t('app', 'Delete client'), [
+        'class' => 'btn btn-warning',
         'data-loading-text' => Yii::t('app', 'Setting...'),
-        'onClick'           => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
+        'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
     ]),
 ]);
 Modal::end();
@@ -258,13 +268,13 @@ echo Html::endForm();
 ?>
 <?php
 Modal::begin([
-    'id'            => 'enable-block-client-form-modal',
-    'header'        => Html::tag('h4', Yii::t('app', 'Block client')),
+    'id' => 'enable-block-client-form-modal',
+    'header' => Html::tag('h4', Yii::t('app', 'Block client')),
     'headerOptions' => ['class' => 'label-warning'],
-    'footer'        => Html::button(Yii::t('app', 'Block'), [
-        'class'             => 'btn btn-warning',
+    'footer' => Html::button(Yii::t('app', 'Block'), [
+        'class' => 'btn btn-warning',
         'data-loading-text' => Yii::t('app', 'Setting...'),
-        'onClick'           => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
+        'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
     ]),
 ]); ?>
 <div id='enable-block-content'></div>
@@ -280,13 +290,13 @@ echo Html::endForm();
 ?>
 <?php
 Modal::begin([
-    'id'            => 'disable-block-client-form-modal',
-    'header'        => Html::tag('h4', Yii::t('app', 'Unblock client')),
+    'id' => 'disable-block-client-form-modal',
+    'header' => Html::tag('h4', Yii::t('app', 'Unblock client')),
     'headerOptions' => ['class' => 'label-warning'],
-    'footer'        => Html::button(Yii::t('app', 'Unblock'), [
-        'class'             => 'btn btn-warning',
+    'footer' => Html::button(Yii::t('app', 'Unblock'), [
+        'class' => 'btn btn-warning',
         'data-loading-text' => Yii::t('app', 'Setting...'),
-        'onClick'           => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
+        'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')")
     ]),
 ]); ?>
 <div id='disable-block-content'></div>
@@ -301,9 +311,9 @@ echo Html::endForm();
         var view = $(this).data('view');
 
         if ( view == '_tariff' )
-            location.replace('".Url::toRoute(['index','tpl'=>'_tariff'])."');
+            location.replace('" . Url::toRoute(['index', 'tpl' => '_tariff']) . "');
         else
-            location.replace('".Url::toRoute(['index','tpl'=>'_card'])."');
+            location.replace('" . Url::toRoute(['index', 'tpl' => '_card']) . "');
     });
 
     $( document ).on('click', '.delete-client', function () {
