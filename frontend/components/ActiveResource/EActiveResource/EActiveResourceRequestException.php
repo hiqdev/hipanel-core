3ece8f7e389fa76cc8d966ced01534cd554e84e2
Andrey Klochok <andreyklochok@gmail.com>
2015-04-15 19:28:23 +0300

    Remove ActiveResource

--- frontend/components/ActiveResource/EActiveResource/EActiveResourceRequestException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace frontend\components\ActiveResource\EActiveResource;
-/**
- * @author Johannes "Haensel" Bauer
- * @since 0.2
- */
-
-class EActiveResourceRequestException extends EActiveResourceException{}
-?>
--- frontend/components/ActiveResource/EActiveResource/EActiveResourceRequestException_Curl.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace frontend\components\ActiveResource\EActiveResource;
-use yii\base\ErrorException;
-class EActiveResourceRequestException_Curl extends ErrorException {}
-?>
