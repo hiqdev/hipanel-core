1c2ed1893a27afab6063c5543868af9b83e5be6e
Andrey Klochok <andreyklochok@gmail.com>
2015-04-29 16:49:34 +0300

    Add some style fix and add JS Function - Scroll To

--- frontend/web/js/panel.js
+++ frontend/web/js/panel.js
@@ -2,4 +2,15 @@
  * Created by tofid on 25.02.15.
  */
 
+/**
+ * Scroll to element
+ * @param element
+ */
+function scrollTo(element, duration) {
+    duration = duration || 500;
+    $('html, body').animate({
+        scrollTop: $(element).offset().top
+    }, duration);
+}
+
 
