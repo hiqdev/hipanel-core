e0e085f192fa6efb2ff99b61719ad0025b09c54b
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-05-14 19:02:55 +0300

    SwitchAction continue deep coding

--- actions/Action.php
+++ actions/Action.php
@@ -7,17 +7,65 @@
 
 namespace hipanel\actions;
 
+use hiqdev\hiar\Collection;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * HiPanel basic action.
- */
+ *
+ * @property Collection collection
+*/
 class Action extends \yii\base\Action
 {
-    protected $_type;
+    /**
+     * @var Action parent called action
+     */
+    public $parent;
+
+    /**
+     * @var Collection|array the options that will be used to create the collection.
+     * Stores collection after creating
+     */
+    protected $_collection;
+
+    /**
+     * Setter for the collection
+     *
+     * @param array $collection config for the collection
+     */
+    public function setCollection($collection)
+    {
+        $this->_collection = $collection;
+    }
+
+    /**
+     * Gets the instance of the collection
+     *
+     * @return Collection
+     */
+    public function getCollection()
+    {
+        if (!is_object($this->_collection)) {
+            $this->_collection = Yii::createObject(ArrayHelper::merge([
+                'class'    => 'hiqdev\hiar\Collection',
+                'model'    => $this->controller->newModel(),
+                'scenario' => $this->controller->action,
+            ], $this->_collection));
+        }
+        return $this->_collection;
+    }
 
     public function getModel()
     {
+        // TODO: getting multiple models
+        return $this->collection->first;
+    }
+
+    /** @inheritdoc */
+    public function getUniqueId()
+    {
+        return $this->parent !== null ? $this->parent->getUniqueId() : $this->controller->getUniqueId() . '/' . $this->id;
     }
 
     public function run()
