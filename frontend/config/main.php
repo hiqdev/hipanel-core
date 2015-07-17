c667bc12388272e2b9331099d62c7969389e4481
Andrii Vasyliev <sol@hiqdev.com>
2015-07-17 14:11:44 +0000

    hi3a site changed to sol-hi3a-master.ahnames.com

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -62,7 +62,7 @@ return [
             'clients' => [
                 'hi3a' => [
                     'class'        => 'hi3a\authclient\Hi3aClient',
-                    'site'         => 'sol-hi3a-new.ahnames.com',
+                    'site'         => 'sol-hi3a-master.ahnames.com',
                     'clientId'     => $params['hi3a_client_id'],
                     'clientSecret' => $params['hi3a_client_secret'],
                 ],
