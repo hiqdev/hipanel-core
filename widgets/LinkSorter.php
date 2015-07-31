ae57b860950d051fd26c8b2ea4392bbce31e2603
Andrey Klochok <andreyklochok@gmail.com>
2015-07-31 13:15:01 +0300

    Create LinkSorter widget and remove old sorter widget

---
+++ widgets/LinkSorter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace hipanel\widgets;
+
+use yii\helpers\Html;
+
+class LinkSorter extends \yii\widgets\LinkSorter
+{
+    public $containerClass = 'dropdown';
+
+    public $buttonClass = 'btn btn-default btn-sm dropdown-toggle';
+
+    public $options = ['class' => 'dropdown-menu', 'role' => 'menu', 'aria-labelledby' => ''];
+
+    /**
+     * Renders the sort links.
+     * @return string the rendering result
+     */
+    protected function renderSortLinks()
+    {
+        $attributes = empty($this->attributes) ? array_keys($this->sort->attributes) : $this->attributes;
+        $links = [];
+        foreach ($attributes as $name) {
+            $links[] = $this->sort->link($name);
+        }
+
+//        return Html::ul($links, array_merge($this->options, ['encode' => false]));
+        return $this->render('LinkSorterView', [
+            'id' => $this->id,
+            'links' => $links,
+            'attributes' => $this->sort->attributes,
+            'options' => array_merge($this->options, ['encode' => false]),
+            'containerClass' => $this->containerClass,
+            'buttonClass' => $this->buttonClass,
+        ]);
+    }
+}
