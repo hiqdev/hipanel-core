295a83c696d26716857fba936b25c3140e8a54b7
Andrey Klochok <andreyklochok@gmail.com>
2015-08-13 11:44:06 +0300

    Add new lang function like Yii

--- common/components/Lang.php
+++ common/components/Lang.php
@@ -13,6 +13,10 @@ class Lang
      * @return string
      */
     public static function l($txt=null, $path='app') {
+        return self::t($txt, $path);
+    }
+
+    public static function t($txt=null, $path='app') {
         if ($txt!==null) {
             return $t=Yii::t($path, str_ireplace(['{lang:','}'], '', $txt));
         }
