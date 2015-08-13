a88275f7198bddf13378d8b96bb9e364cc60ef6a
Andrii Vasyliev <sol@hiqdev.com>
2015-08-13 08:29:05 +0000

    * Gravatar widget: allowed empty email

--- widgets/Gravatar.php
+++ widgets/Gravatar.php
@@ -7,25 +7,26 @@
 
 namespace hipanel\widgets;
 
-use yii\base\InvalidConfigException;
-
 class Gravatar extends \cebe\gravatar\Gravatar
 {
-    public $emailHash;
+    protected $_emailHash;
 
-    private $_emailHash;
+    public function setEmailHash($hash)
+    {
+        $this->_emailHash = $hash;
+    }
 
     /**
      * @return string
-     * @throws InvalidConfigException
      */
     public function getEmailHash()
     {
-        if ($this->_emailHash !== null) {
-            return $this->_emailHash;
-        } elseif ($this->email === null && $this->emailHash === null) {
-            throw new InvalidConfigException('No email and emailHash specified for Gravatar image Widget.');
-        }
-        return $this->_emailHash = ($this->emailHash) ? $this->emailHash : md5(strtolower(trim($this->email)));
+        return $this->_emailHash ?: self::hashEmail($this->email);
+    }
+
+    static public function hashEmail($email)
+    {
+        $email = strtolower(trim($email));
+        return strpos($email, '@') === false ? $email : md5($email);
     }
 }
