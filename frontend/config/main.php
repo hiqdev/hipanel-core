5e3cffecf8a93547f1a66f809740e0e97693e127
Andrii Vasyliev <sol@hiqdev.com>
2015-08-13 08:28:29 +0000

    improved d(): + pre

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,7 +12,7 @@ $params = array_merge(
     require(__DIR__ . '/params-local.php')
 );
 
-function d ($a) { var_dump($a); debug_print_backtrace(0,3); die(); }
+function d ($a) { print "<pre>"; var_dump($a); debug_print_backtrace(0,3); die(); }
 
 return [
     'id'                  => 'hipanel',
