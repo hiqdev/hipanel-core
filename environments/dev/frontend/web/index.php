9e652be0d42a937cdc18de0882e3917bdb8681c6
Andrii Vasyliev <sol@hiqdev.com>
2015-08-15 03:56:59 +0000

    * index.php

--- environments/dev/frontend/web/index.php
+++ environments/dev/frontend/web/index.php
@@ -4,14 +4,16 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../common/config/bootstrap.php');
-require(__DIR__ . '/../config/bootstrap.php');
+require(__DIR__ . '/../../vendor/hiqdev/hipanel/common/config/bootstrap.php');
+require(__DIR__ . '/../../vendor/hiqdev/hipanel/frontend/config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../vendor/hiqdev/hipanel/common/config/main.php'),
     require(__DIR__ . '/../../common/config/main.php'),
     require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+    require(__DIR__ . '/../../vendor/hiqdev/hipanel/frontend/config/main.php'),
+    require(__DIR__ . '/../../frontend/config/main.php'),
+    require(__DIR__ . '/../../frontend/config/main-local.php')
 );
 
 $application = new yii\web\Application($config);
