5a78957b1091456fdf84a999d0b443cc3f6e0139
Andrii Vasyliev <sol@hiqdev.com>
2015-08-16 12:56:55 +0000

    still making configs

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_url'             => 'https://hiapi.example.com',
-    'hiam_site'           => 'hiam.example.com',
-    'hiam_client_id'      => 'example',
-    'hiam_client_secret'  => 'example',
+    'api_url'            => 'https://hiapi.example.com',
+    'hiam_site'          => 'hiam.example.com',
+    'hiam_client_id'     => 'example',
+    'hiam_client_secret' => 'example',
 ];
