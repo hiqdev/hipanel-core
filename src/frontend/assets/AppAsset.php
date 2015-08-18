414859e775627183a913d50e9ce9dca7bd4a4670
Andrii Vasyliev <sol@hiqdev.com>
2015-08-18 14:30:04 +0000

    fixed frontend/assets/AppAsset to work

--- src/frontend/assets/AppAsset.php
+++ src/frontend/assets/AppAsset.php
@@ -1,22 +1,12 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace hipanel\frontend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@hipanel/frontend/assets/AppAssetFiles';
     public $css = [
         '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
         '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
