ea8254c21f1834aa352eac11be9b1a0b9c5ec1ce
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-08-18 16:13:22 +0000

    Changed default columns definition in GridView

--- src/actions/BootstrapSwitchAction.php
+++ src/actions/BootstrapSwitchAction.php
@@ -2,13 +2,7 @@
 
 namespace hipanel\actions;
 
-use hiqdev\hiart\Collection;
 use Yii;
-use yii\base\Action;
-use yii\console\Response;
-use yii\helpers\ArrayHelper;
-use yii\helpers\VarDumper;
-use yii\web\NotFoundHttpException;
 
 class BootstrapSwitchAction extends SwitchAction
 {
