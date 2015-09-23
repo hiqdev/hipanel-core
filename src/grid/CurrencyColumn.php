0f73864e4faf52a08ef738cdeec371fe811e49ab
Andrii Vasyliev <sol@hiqdev.com>
2015-09-23 08:45:04 +0000

    + colors substitution for CurrencyColumn

--- src/grid/CurrencyColumn.php
+++ src/grid/CurrencyColumn.php
@@ -13,17 +13,23 @@ class CurrencyColumn extends DataColumn
     public $format = 'html';
     public $filter = false;
     public $compare = false;
+    public $colors  = [];
 
     public $urlCallback;
 
+    public function getColor($type)
+    {
+        return $this->colors[$type] ?: $type;
+    }
+
     public function getDataCellValue ($model, $key, $index) {
         $value = $model->{$this->attribute};
-        $class = 'info';
+        $class = $this->getColor('success');
         if ($value<0) {
-            $class = 'warning';
+            $class = $this->getColor('warning');
         };
         if ($value < -($model->{$this->compare} ?: 0)) {
-            $class = 'danger';
+            $class = $this->getColor('danger');
         };
         $url = $this->urlCallback ? call_user_func($this->urlCallback, $model, $key, $index) : null;
         $txt = Yii::$app->formatter->format($value, ['currency', $model->currency]);
