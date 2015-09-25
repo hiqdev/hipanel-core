e87370fcc022c6682c8530cf97aba0a1875cb15f
Andrii Vasyliev <sol@hiqdev.com>
2015-09-25 08:57:17 +0000

    improved Gravatar to draw default Gravatar icon when no email or hash given

--- src/widgets/Gravatar.php
+++ src/widgets/Gravatar.php
@@ -23,12 +23,24 @@ class Gravatar extends \cebe\gravatar\Gravatar
      */
     public function getEmailHash()
     {
-        return $this->_emailHash ?: self::hashEmail($this->email);
+        if (!$this->_emailHash) {
+            $this->_emailHash = self::hashEmail($this->email);
+        }
+
+        return $this->_emailHash;
     }
 
     static public function hashEmail($email)
     {
         $email = strtolower(trim($email));
         return strpos($email, '@') === false ? $email : md5($email);
+    }
+
+    public function run()
+    {
+        if (!$this->emailHash) {
+            $this->defaultImage = '';
+        }
+        return parent::run();
     }
 }
