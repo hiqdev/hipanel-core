d9a46cc54a2fde6b1ea6607d41f6fbc7fbdb0021
Andrii Vasyliev <sol@hiqdev.com>
2015-10-06 12:08:33 +0000

    + DomainValidator, moved from domain module

---
+++ src/validators/DomainValidator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace hipanel\validators;
+
+/**
+ * Class DomainValidator is used to validate domain names with a regular expression
+ */
+class DomainValidator extends \yii\validators\RegularExpressionValidator
+{
+    /**
+     * @inheritdoc
+     */
+    public $pattern = '/^([a-z0-9][a-z0-9-]*\.)+[a-z0-9][a-z0-9-]*$/';
+
+    /**
+     * @inheritdoc
+     */
+    public function init () {
+        $this->message = \Yii::t('app', '{attribute} does not look like a valid domain name');
+    }
+}
