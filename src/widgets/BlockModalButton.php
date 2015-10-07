42544156cbc46faf52bd64d49f495f3d66f8e3d3
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-10-07 19:00:50 +0300

    Widget Block reimplemented and renamed to BlockModalButton, minor

---
+++ src/widgets/BlockModalButton.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace hipanel\widgets;
+
+use Yii;
+use hipanel\base\Model;
+use hipanel\helpers\ArrayHelper;
+use hipanel\helpers\FontIcon;
+use yii\base\Event;
+use yii\base\Widget;
+use yii\bootstrap\Modal;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+class BlockModalButton extends Widget
+{
+    const ACTION_ENABLE = 'enable';
+    const ACTION_DISABLE = 'disable';
+
+    const SCENARIO_ENABLE = 'enable-block';
+    const SCENARIO_DISABLE = 'disable-block';
+
+    const EVENT_BEFORE_BODY = 'beforeBody';
+    const EVENT_AFTER_BODY = 'afterBody';
+
+    /**
+     * @var integer ID of action
+     *
+     * @see ACTION_DISABLE
+     * @SEE ACTION_ENABLE
+     */
+    public $action = self::ACTION_DISABLE;
+
+    /**
+     * @var Model
+     */
+    public $model;
+
+    /**
+     * @var string
+     */
+    public $scenario;
+
+    /**
+     * @var array|Modal stores the overriding options for [[ModalButton]].
+     * After Modal creating, stores the object.
+     */
+    public $modal = [];
+
+    /**
+     * @var string the validation URL, will be passed to [[ModalButton::form]]
+     * Default: `validate-form?scenario={$this->scenario}`
+     */
+    public $validationUrl;
+
+    /**
+     * @var array block reasons. Should be a key-value array, will be passed to [[Html::dropDown]]
+     * Default: `Yii::$app->controller->getBlockReasons()`
+     */
+    public $blockReasons;
+
+    /**
+     * @var array Options for trigger button. Will be passed to [[ModalButton::button]]
+     */
+    public $button = [];
+
+    /**
+     * @var array Options to render the header of modal.
+     * Keys with special behaviour:
+     *  - tag - html tag, used to wrap the header label (default: h4)
+     *  - label - the value of tag
+     *
+     * All other options will be passed as third argument options to [[Html::tag]]
+     */
+    public $header = [];
+
+    /**
+     * @var array Options to render the warning message inside of the modal.
+     * Keys with special behaviour:
+     *  - tag - html tag, used to wrap the label (default: h4)
+     *  - label - the value of tag
+     *
+     * All other options will be passed as third argument options to [[Html::tag]]
+     */
+    public $warning = [];
+
+    /**
+     * @var array
+     */
+    public $footer = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->initOptions();
+    }
+
+    /**
+     * Configures options of widget
+     */
+    public function initOptions()
+    {
+        if ($this->scenario === null) {
+            $this->scenario = $this->action === static::ACTION_ENABLE ? static::SCENARIO_ENABLE : static::SCENARIO_DISABLE;
+        }
+
+        if ($this->validationUrl === null) {
+            $this->validationUrl = Url::toRoute(['validate-form', 'scenario' => $this->scenario]);
+        }
+
+        if ($this->blockReasons === null) {
+            $this->blockReasons = Yii::$app->controller->getBlockReasons();
+        }
+
+        if (is_array($this->button)) {
+            $this->button = array_replace([
+                static::ACTION_ENABLE => [
+                    'label' => FontIcon::i('fa-lock fa-2')->fixedWidth() . Yii::t('app', 'Enable block'),
+                    'position' => ModalButton::BUTTON_OUTSIDE,
+                ],
+                static::ACTION_DISABLE => [
+                    'label' => FontIcon::i('fa-lock fa-2')->fixedWidth() . Yii::t('app', 'Disable block'),
+                    'position' => ModalButton::BUTTON_OUTSIDE,
+                ],
+            ], $this->button);
+            $this->button = $this->button[$this->action];
+        }
+
+        if (is_array($this->header)) {
+            $this->header = ArrayHelper::merge([
+                static::ACTION_ENABLE => [
+                    'label' => Yii::t('app', 'Are you sure you want to enable block'),
+                    'class' => 'label-danger'
+                ],
+                static::ACTION_DISABLE => [
+                    'label' => Yii::t('app', 'Are you sure you want to disable block'),
+                    'class' => 'label-info'
+                ],
+            ], $this->header);
+
+            $this->header = $this->header[$this->action];
+        }
+
+        if (is_array($this->warning)) {
+            $this->warning = ArrayHelper::merge([
+                static::ACTION_ENABLE => false,
+                static::ACTION_DISABLE => [
+                    'label' => Yii::t('app', 'Check whether all of the violations were eliminated')
+                ]
+            ], $this->warning);
+
+            $this->warning = $this->warning[$this->action];
+        }
+
+        if (is_array($this->footer)) {
+            $this->footer = ArrayHelper::merge([
+                static::ACTION_ENABLE => [
+                    'label' => Yii::t('app', 'Enable block'),
+                    'data-loading-text' => Yii::t('app', 'Enabling block...'),
+                    'class' => 'btn btn-danger',
+                ],
+                static::ACTION_DISABLE => [
+                    'label' => Yii::t('app', 'Disable block'),
+                    'data-loading-text' => Yii::t('app', 'Disabling block...'),
+                    'class' => 'btn btn-info',
+                ]
+            ], $this->footer);
+
+            $this->footer = $this->footer[$this->action];
+        }
+    }
+
+    /**
+     * Begins modal
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function modalBegin()
+    {
+        $config = ArrayHelper::merge([
+            'class' => ModalButton::className(),
+            'model' => $this->model,
+            'scenario' => $this->scenario,
+            'button' => $this->button,
+            'form' => [
+                'enableAjaxValidation' => true,
+                'validationUrl' => $this->validationUrl,
+            ],
+            'modal' => [
+                'header' => Html::tag(ArrayHelper::remove($this->header, 'tag', 'h4'),
+                    ArrayHelper::remove($this->header, 'label')),
+                'headerOptions' => $this->header,
+                'footer' => $this->footer,
+            ],
+        ], $this->modal);
+
+        $this->modal = call_user_func([ArrayHelper::remove($config, 'class'), 'begin'], $config);
+    }
+
+    /**
+     * Ends modal
+     */
+    protected function modalEnd()
+    {
+        $this->modal->end();
+    }
+
+    /**
+     * Renders the body of the Modal.
+     * Triggers [[EVENT_BEFORE_BODY]] and [[EVENT_AFTER_BODY]]
+     * Set `$event->handled = true` to prevent default body render.
+     */
+    protected function renderBody()
+    {
+        $event = new Event();
+        $this->trigger(static::EVENT_BEFORE_BODY, $event);
+        if ($event->handled) {
+            return;
+        }
+
+        if ($this->warning) {
+            echo Html::tag('div',
+                Html::tag(
+                    ArrayHelper::remove($this->warning, 'tag', 'h4'),
+                    ArrayHelper::remove($this->warning, 'label'),
+                    $this->warning
+                ),
+                ['class' => 'callout callout-warning']
+            );
+        }
+
+        if ($this->blockReasons) {
+            echo $this->modal->form->field($this->model, 'type')->dropDownList($this->blockReasons);
+        }
+        echo $this->modal->form->field($this->model, 'comment');
+
+        $this->trigger(static::EVENT_AFTER_BODY);
+    }
+
+    /**
+     * Renders widget
+     */
+    public function run()
+    {
+        $this->modalBegin();
+        $this->renderBody();
+        $this->modalEnd();
+    }
+}
