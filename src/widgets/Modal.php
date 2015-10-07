900e4089bff36f755e7b7197d3c6b8d2bf661b16
Andrii Vasyliev <sol@hiqdev.com>
2015-10-07 08:24:27 +0000

    redone Modal widget

--- src/widgets/Modal.php
+++ src/widgets/Modal.php
@@ -11,15 +11,67 @@ use yii\web\JsExpression;
 
 class Modal extends \yii\bootstrap\Modal
 {
+    protected $_actionUrl;
+
+    public function setActionUrl($url)
+    {
+        $this->_actionUrl = $url;
+    }
+
+    public function getActionUrl()
+    {
+        return $this->_actionUrl ? Url::to($this->_actionUrl) : Url::to($this->scenario);
+    }
+
+    protected $_modalFormId;
+
+    public function setModalFormId($id)
+    {
+        $this->_modalFormId = $id;
+    }
+
+    public function getModalFormId()
+    {
+        return $this->_modalFormId ?: $this->scenario . '-form';
+    }
+
+    protected $_errorText;
+
+    public function setErrorText($text)
+    {
+        $this->_errorText = $text;
+    }
+
+    public function getErrorText()
+    {
+        return $this->_errorText ? : Yii::t('app', 'An error occurred. Try again later.');
+    }
+
+    protected $_successText;
+
+    public function setSuccessText($text)
+    {
+        $this->_successText = $text;
+    }
+
+    public function getSuccessText()
+    {
+        return $this->_successText ? : Yii::t('app', 'Settings saved');
+    }
+
+    protected $_loadingText;
+
+    public function setLoadingText($text)
+    {
+        $this->_loadingText = $text;
+    }
+
+    public function getLoadingText()
+    {
+        return $this->_loadingText ?: Yii::t('app', 'loading') . '...';
+    }
+
     public $scenario;
-
-    public $actionUrl = [];
-
-    public $modalFormId;
-
-    public $errorText;
-
-    public $successText;
 
     public function init()
     {
@@ -32,39 +84,22 @@ class Modal extends \yii\bootstrap\Modal
         parent::init();
     }
 
-    private function getActionUrl()
-    {
-        return $this->actionUrl ? Url::to($this->actionUrl) : Url::to($this->scenario);
-    }
-
-    private function getModalFormId()
-    {
-        return $this->modalFormId ? : $this->scenario . '-form';
-    }
-
     protected function initPerformPjaxSubmit()
     {
-        $view = Yii::$app->view;
-        $buttonLoadingText = Yii::t('app', 'loading');
-        $formId = $this->getModalFormId();
-        $formActionUrl = $this->getActionUrl();
-        $modalId = $this->getId();
-        $errorText = ($this->errorText) ? : Yii::t('app', 'An error occurred. Try again later.');
-        $successText = ($this->successText) ? : Yii::t('app', 'The settings saved');
-        $view->registerJs(<<<JS
+        Yii::$app->view->registerJs(<<<JS
 //            jQuery(document).on('pjax:beforeSend', function() {
-//                jQuery('form[data-pjax] button').button('{$buttonLoadingText}');
+//                jQuery('form[data-pjax] button').button('{$this->loadingText}');
 //            });
 //            jQuery(document).on('pjax:end', function() {
 //                jQuery('form[data-pjax] button').button('reset');
 //            });
 
-            jQuery(document).on('submit', '#{$formId}', function(event) {
+            jQuery(document).on('submit', '#{$this->modalFormId}', function(event) {
                 event.preventDefault();
                 var form = jQuery(this);
-                var btn = jQuery('form[data-pjax] button').button('{$buttonLoadingText}');
+                var btn = jQuery('form[data-pjax] button').button('{$this->loadingText}');
                 jQuery.ajax({
-                    url: '{$formActionUrl}',
+                    url: '{$this->actionUrl}',
                     type: 'POST',
                     //dataType: 'json',
                     timeout: 0,
@@ -72,7 +107,7 @@ class Modal extends \yii\bootstrap\Modal
                     error: function() {
                         new PNotify({
                             title: 'Error',
-                            text: "{$errorText}",
+                            text: "{$this->errorText}",
                             type: 'error',
                             buttons: {
                                 sticker: false
@@ -81,10 +116,10 @@ class Modal extends \yii\bootstrap\Modal
                         });
                     },
                     success: function() {
-                        jQuery('#$modalId').modal('hide');
+                        jQuery('#$this->id').modal('hide');
                         new PNotify({
                             title: 'Success',
-                            text: "{$successText}",
+                            text: "{$this->successText}",
                             type: 'info',
                             buttons: {
                                 sticker: false
@@ -101,30 +136,29 @@ JS
 
     protected function initAdditionalOptions()
     {
-        $actionUrl = $this->getActionUrl();
-        $modalId = $this->getId();
-        $loadingHtml = Json::htmlEncode($this->getLoadHtml());
+        $quotedHtml = Json::htmlEncode($this->loadingHtml);
         $this->clientEvents['show.bs.modal'] = new JsExpression("function() {
-            jQuery('#{$modalId} .modal-body').load('{$actionUrl}');
+            jQuery('#{$this->id} .modal-body').load('{$this->actionUrl}');
         }");
         $this->clientEvents['hidden.bs.modal'] = new JsExpression("function() {
-            jQuery('#{$modalId} .modal-body').html({$loadingHtml});
+            jQuery('#{$this->id} .modal-body').html({$quotedHtml});
         }");
     }
 
     protected function renderBodyBegin()
     {
-        return Html::beginTag('div', ['class' => 'modal-body']) . $this->getLoadHtml();
+        return Html::beginTag('div', ['class' => 'modal-body']) . $this->loadingHtml;
     }
 
-    protected function getLoadHtml()
+    public function getLoadingHtml()
     {
-        return '
+        return <<<HTML
         <div class="progress">
             <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
-                <span class="sr-only">' . Yii::t('app', 'loading') . '...</span>
+                <span class="sr-only">{$this->loadingText}</span>
             </div>
         </div>
-        ';
+HTML
+        ;
     }
 }
