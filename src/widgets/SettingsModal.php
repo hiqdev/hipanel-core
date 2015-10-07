a8c341504ff30258915d7db152097120f66063e3
Andrii Vasyliev <sol@hiqdev.com>
2015-10-07 08:24:38 +0000

    + SettingsModal widget

---
+++ src/widgets/SettingsModal.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace hipanel\widgets;
+
+use Yii;
+use yii\helpers\Html;
+use hipanel\helpers\FontIcon;
+
+class SettingsModal extends Modal {
+    public $model;
+
+    public $title;
+
+    public $icon;
+
+    public function init()
+    {
+        $this->header = Html::tag('h4', $this->title, ['class' => 'modal-title']);
+        $this->actionUrl = [$this->scenario, 'id' => $this->model->id];
+        $this->toggleButton = [
+            'tag'   => 'a',
+            'label' => FontIcon::i($this->icon) . $this->title,
+            'class' => 'clickable',
+        ];
+        parent::init();
+    }
+}
