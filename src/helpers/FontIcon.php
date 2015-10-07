f5378f4b61abec3810a2fcc1b23540aacf5ad5b2
Andrii Vasyliev <sol@hiqdev.com>
2015-10-07 08:23:57 +0000

    + FontIcon helper

---
+++ src/helpers/FontIcon.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace hipanel\helpers;
+
+use Yii;
+use yii\helpers\Html;
+
+class FontIcon extends \yii\base\Object
+{
+
+    public $tag;
+
+    public $content;
+
+    public $options = [];
+
+    public static function i($name, $params = [])
+    {
+        return self::build('i', $name, $params);
+    }
+
+    public static function build($tag, $name, $params = [])
+    {
+        $class = get_called_class();
+        return Yii::createObject(compact('class', 'tag', 'name', 'params'));
+    }
+    
+    public function __toString()
+    {
+        return $this->render();
+    }
+
+    public function addClass($class)
+    {
+        Html::addCssClass($this->options, $class);
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->addClass(self::nameClass($name));
+    }
+
+    public function setRotate($angle)
+    {
+        $this->rotate($angle);
+    }
+
+    public function rotate($angle)
+    {
+        return $this->addClass("fa-rotate-$angle");
+    }
+
+    public function border()
+    {
+        return $this->addClass('fa-border');
+    }
+
+    public function fixedWidth()
+    {
+        return $this->addClass('fa-fw');
+    }
+
+    public static function nameClass($name)
+    {
+        return (substr($name, 0, 3) === 'fa-' ? 'fa ' : '') . $name;
+    }
+
+    public function setParams($params)
+    {
+        foreach ($params as $k => $v) {
+            $this->{$k} = $v;
+        }
+    }
+
+    public function render()
+    {
+        return Html::tag($this->tag, $this->content, $this->options);
+    }
+}
