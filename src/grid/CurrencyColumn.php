70f79468a38282286791e39859e8ecff42e55272
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-10-08 15:27:45 +0300

    CurrencyColumn code style enhancements

--- src/grid/CurrencyColumn.php
+++ src/grid/CurrencyColumn.php
@@ -2,18 +2,28 @@
 
 namespace hipanel\grid;
 
+use hipanel\base\Model;
 use Yii;
 use yii\helpers\Html;
-use yii\i18n\Formatter;
 
+/**
+ * Class CurrencyColumn
+ * @package hipanel\grid
+ */
 class CurrencyColumn extends DataColumn
 {
     public $attribute = 'balance';
     public $nameAttribute = 'balance';
     public $format = 'html';
     public $filter = false;
+
+    /**
+     * @var bool|string Whether to compare [[attribute]] with another attribute to change the display colors
+     *  - boolean false - do not compare
+     *  - string - name of attribute to compare with
+     */
     public $compare = false;
-    public $colors  = [];
+    public $colors = [];
 
     public $urlCallback;
 
@@ -27,15 +37,26 @@ class CurrencyColumn extends DataColumn
         return $this->urlCallback ? call_user_func($this->urlCallback, $model, $key, $index) : null;
     }
 
-    public function getDataCellValue ($model, $key, $index) {
-        $value = $model->{$this->attribute};
-        $color = $value==0 ? 'primary' : 'success';
-        if ($value<0) {
+    /**
+     *
+     * @param Model $model
+     * @param mixed $key
+     * @param int $index
+     * @return string
+     */
+    public function getDataCellValue($model, $key, $index)
+    {
+        $value = $model->getAttribute($this->attribute);
+        $color = $value == 0 ? 'primary' : 'success';
+
+        if ($value < 0) {
             $color = 'warning';
         };
-        if ($value < -($model->{$this->compare} ?: 0)) {
+
+        if ($value < -($model->getAttribute($this->compare) ?: 0)) {
             $color = 'danger';
-        };
+        }
+
         $url = $this->getUrl($model, $key, $index);
         $txt = Yii::$app->formatter->format($value, ['currency', $model->currency]);
         $ops = ['class' => 'text-' . $this->getColor($color)];
