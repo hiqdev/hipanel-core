1bd57bd86f22a5570806299b68bbad37e9282415
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-11-07 15:25:00 +0200

    Added ResizableColumnsAsset to AppAsset

--- src/frontend/assets/AppAsset.php
+++ src/frontend/assets/AppAsset.php
@@ -20,5 +20,9 @@ class AppAsset extends AssetBundle
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
         'hiqdev\assets\select2_bootstrap3_css\Select2Bootstrap3CssAsset',
+        // Adds Resizable columns JS and CSS to all pages
+        // This is a temporary fix because of https://github.com/yiisoft/yii2/issues/2310
+        // On Pjax page loading, ajax prefilter removes all CSS styles that are not on the main page
+        'hiqdev\yii2\assets\JqueryResizableColumns\ResizableColumnsAsset',
     ];
 }
