61c62e12fe33027bb282b9645be723320e749587
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2015-11-17 18:45:02 +0200

    SmartDeleteAction now accepts data from GET valiables in POST request

--- src/actions/SmartDeleteAction.php
+++ src/actions/SmartDeleteAction.php
@@ -22,4 +22,13 @@ class SmartDeleteAction extends SmartPerformAction
             ],
         ], 'first');
     }
+
+    public function loadCollection($data = null)
+    {
+        // Fixes delete buttons in GridView.
+        // When button in being pressed, the system submits POST request which contains
+        // POST model ClassSearch and GET attribute with ID
+        $data = Yii::$app->request->get() ? [Yii::$app->request->get()] : null;
+        parent::loadCollection($data);
+    }
 }
