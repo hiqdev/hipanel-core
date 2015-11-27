c49b1f42167e98a11a1349019d955aa79d7bf58b
Andrey Klochok <andreyklochok@gmail.com>
2015-11-27 14:06:29 +0200

    Change FontAwesome version

--- src/frontend/assets/AppAsset.php
+++ src/frontend/assets/AppAsset.php
@@ -8,7 +8,7 @@ class AppAsset extends AssetBundle
 {
     public $sourcePath = '@hipanel/frontend/assets/AppAssetFiles';
     public $css = [
-        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+        '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
         '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
         'css/panel.css',
     ];
