948c4d31268882f6a20eaa7e2ce55b7e9d6d266c
Andrey Klochok <andreyklochok@gmail.com>
2015-12-17 19:00:06 +0200

    Add Octicons Asset

---
+++ src/frontend/assets/OcticonsAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hipanel\frontend\assets;
+
+use yii\web\AssetBundle;
+
+class OcticonsAsset extends AssetBundle
+{
+//    public $sourcePath = '@bower/octicons';
+
+    public $css = [
+    ];
+}
+
