815c325dd8f7e3843eeccd909a168db86239422f
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-01-22 22:02:31 +0200

    FilterStorage implemented in IndexAction

--- src/actions/IndexAction.php
+++ src/actions/IndexAction.php
@@ -3,6 +3,7 @@
 namespace hipanel\actions;
 
 use Closure;
+use hipanel\base\FilterStorage;
 use hipanel\base\Model;
 use Yii;
 use yii\helpers\ArrayHelper;
@@ -17,6 +18,11 @@ class IndexAction extends SearchAction
      * @var string view to render.
      */
     public $view = 'index';
+
+    /**
+     * @var array The map of filters for the [[hipanel\base\FilterStorage|FilterStorage]]
+     */
+    public $filterStorageMap = [];
 
     public function init()
     {
@@ -47,4 +53,30 @@ class IndexAction extends SearchAction
         ]);
     }
 
+    /**
+     * @inheritdoc
+     */
+    public function getDataProvider()
+    {
+        if ($this->dataProvider === null) {
+            $formName = $this->getSearchModel()->formName();
+            $requestFilters = Yii::$app->request->get($formName) ?: Yii::$app->request->get() ?: Yii::$app->request->post();
+
+            $filterStorage = new FilterStorage(['map' => $this->filterStorageMap]);
+            $filters = $filterStorage->get();
+
+            $search = ArrayHelper::merge($this->findOptions, $filters, $requestFilters);
+
+            $filterStorage->set($requestFilters);
+
+            $this->returnOptions[$this->controller->modelClassName()] = ArrayHelper::merge(
+                ArrayHelper::remove($search, 'return', []),
+                ArrayHelper::remove($search, 'rename', [])
+            );
+
+            $this->dataProvider = $this->getSearchModel()->search([$formName => $search], $this->dataProviderOptions);
+        }
+
+        return $this->dataProvider;
+    }
 }
