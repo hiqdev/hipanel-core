e7ec391ad0c9cf15d289dee1a23c38937c1747ea
Andrii Vasyliev <sol@hiqdev.com>
2016-01-26 15:06:00 +0000

    removed old invalid tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * HiPanel core package
+ *
+ * @link      https://hipanel.com/
+ * @package   hipanel-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
