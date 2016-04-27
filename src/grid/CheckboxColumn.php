7700d0b2f89a565395c8c2b10185c26e48779413
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-04-27 18:51:01 +0300

    CheckboxColumn - added parent::registerClientScript() call

--- src/grid/CheckboxColumn.php
+++ src/grid/CheckboxColumn.php
@@ -15,9 +15,17 @@ use hiqdev\higrid\FeaturedColumnTrait;
 
 class CheckboxColumn extends \yii\grid\CheckboxColumn
 {
-    use FeaturedColumnTrait;
+    use FeaturedColumnTrait {
+        registerClientScript as traitRegisterClientScript;
+    }
 
     public $attribute;
+
+    public function registerClientScript()
+    {
+        parent::registerClientScript();
+        $this->traitRegisterClientScript();
+    }
 
     /** {@inheritdoc} */
     public $defaultOptions = [
