fe81de8e0966f4c099e192c00a3efd0e853feaa6
tofid <andreyklochok@gmail.com>
2016-04-28 19:33:38 +0300

    Fix layout

--- src/widgets/views/IndexPage/vertical.php
+++ src/widgets/views/IndexPage/vertical.php
@@ -4,24 +4,19 @@ $widget = $this->context;
 
 ?>
 
-<div class="box">
-    <div class="box-header with-border">
-        <?= $widget->renderContent('main-actions') ?>
-        <?= $widget->renderSearchButton() ?>
-        <?= $widget->renderContent('show-actions') ?>
-        <div class="row">
-            <div class="col-md-12">
-                <?= $widget->renderSearchForm() ?>
-            </div>
-        </div>
-
-        <div class="box-bulk-actions box-tools pull-right">
-            <fieldset disabled="disabled">
-                <?= $widget->renderContent('bulk-actions') ?>
-            </fieldset>
-        </div>
-    </div>
+<div class="box box-primary">
     <div class="box-body no-padding">
+        <div class="mailbox-controls">
+            <?= $widget->renderContent('main-actions') ?>
+            <?= $widget->renderSearchButton() ?>
+            <?= $widget->renderContent('show-actions') ?>
+            <div class="box-tools box-bulk-actions pull-right">
+                <fieldset disabled="disabled">
+                    <?= $widget->renderContent('bulk-actions') ?>
+                </fieldset>
+            </div>
+            <?= $widget->renderSearchForm() ?>
+        </div>
         <?= $widget->renderContent('table') ?>
     </div>
 </div>
