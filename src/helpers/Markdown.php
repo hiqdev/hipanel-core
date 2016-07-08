94ad2bb97d9d9c682ffb7b689d7e864df860a159
Andrii Vasyliev <sol@hiqdev.com>
2016-07-08 16:27:27 +0000

    added hipanel Markdown

---
+++ src/helpers/Markdown.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * HiPanel core package
+ *
+ * @link      https://hipanel.com/
+ * @package   hipanel-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\helpers;
+
+class Markdown extends \yii\helpers\Markdown
+{
+    public static $defaultFlavor = 'gfm-comment';
+}
