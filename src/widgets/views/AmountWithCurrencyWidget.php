1f5d0b4d69ebee66da7f13e834a138eb34f751d3
Andrii Vasyliev <sol@hiqdev.com>
2016-07-13 14:03:33 +0000

    minor

--- src/widgets/views/AmountWithCurrencyWidget.php
+++ src/widgets/views/AmountWithCurrencyWidget.php
@@ -4,7 +4,6 @@ use hipanel\helpers\StringHelper;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
-$currencySymbol = Html::getAttributeValue($model, $selectAttribute);
 $this->registerJs(<<<JS
     $('#$widgetId .dropdown-menu a').on('click', function (event) {
         var elem = $(this);
@@ -16,18 +15,18 @@ JS
 ?>
 <div id="<?= $widgetId ?>" class="input-group amount-with-currency-widget">
     <div class="input-group-btn">
-        <button type="button" class="btn btn-default iwd-label">
-            <?= StringHelper::getCurrencySymbol($currencySymbol) ?>
+        <button type="button" class="btn btn-default iwd-label dropdown-toggle" data-toggle="dropdown">
+            <?= StringHelper::getCurrencySymbol(Html::getAttributeValue($model, $selectAttribute)) ?>
         </button>
         <button type=button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <span class="caret"></span> <span class="sr-only"><?= Yii::t('app', 'Toggle Dropdown') ?></span>
         </button>
         <ul class="dropdown-menu">
             <?php foreach (ArrayHelper::remove($selectAttributeOptions, 'items', []) as $k => $v) : ?>
-            <li><?= Html::a(StringHelper::getCurrencySymbol($k), '#', ['data-value' => $k, 'data-label' => StringHelper::getCurrencySymbol($k)]) ?>
-                <?php endforeach ?>
+                <li><?= Html::a(StringHelper::getCurrencySymbol($k), '#', ['data-value' => $k, 'data-label' => StringHelper::getCurrencySymbol($k)]) ?>
+            <?php endforeach ?>
         </ul>
     </div>
     <?= Html::activeInput($inputAttributeType, $model, $attribute, array_merge(['class' => 'form-control'], $inputOptions)) ?>
-    <?= Html::activeHiddenInput($model, $selectAttribute, $selectAttributeOptions) ?>
+    <?= Html::activeHiddenInput($model, $selectAttribute, array_merge(['class' => 'form-control'], $selectAttributeOptions)) ?>
 </div>
