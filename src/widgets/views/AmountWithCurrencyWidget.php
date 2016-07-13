8b82a79778d44d3bbc39acbf0446bf6b358d2507
tofid <andreyklochok@gmail.com>
2016-07-13 16:54:55 +0300

    Fixed default currency set from js

--- src/widgets/views/AmountWithCurrencyWidget.php
+++ src/widgets/views/AmountWithCurrencyWidget.php
@@ -5,19 +5,11 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
 $currencySymbol = Html::getAttributeValue($model, $selectAttribute);
-if ($currencySymbol === null) {
-    $selectAttributeOptions = array_merge($selectAttributeOptions, ['value' => 'usd']);
-}
 $this->registerJs(<<<JS
     $('#$widgetId .dropdown-menu a').on('click', function (event) {
         var elem = $(this);
         $('#$widgetId .iwd-label').text(elem.data('label'));
         $('#$widgetId :hidden').val(elem.data('value'));
-    });
-    // Add default currency after add new block
-    $(".dynamicform_wrapper").on("afterInsert", function(e, item) {
-        var currencyInput = $(':input:last', $(item)), currencyValue = '$currencySymbol';
-        if (currencyValue === '') currencyInput.val('usd');
     });
 JS
 );
@@ -25,7 +17,7 @@ JS
 <div id="<?= $widgetId ?>" class="input-group amount-with-currency-widget">
     <div class="input-group-btn">
         <button type="button" class="btn btn-default iwd-label">
-            <?= StringHelper::getCurrencySymbol($currencySymbol !== null ? $currencySymbol : 'usd') ?>
+            <?= StringHelper::getCurrencySymbol($currencySymbol) ?>
         </button>
         <button type=button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <span class="caret"></span> <span class="sr-only"><?= Yii::t('app', 'Toggle Dropdown') ?></span>
