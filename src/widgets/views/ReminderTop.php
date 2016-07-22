4a54f32c6fdca51732c5bbc56b6e62a04f3137b2
tofid <andreyklochok@gmail.com>
2016-07-22 21:03:40 +0300

    Reminder to design

--- src/widgets/views/ReminderTop.php
+++ src/widgets/views/ReminderTop.php
@@ -3,9 +3,54 @@ use yii\helpers\Html;
 /* @var integer $count */
 /* @var array $reminders */
 /* @var \hipanel\models\Reminder $reminder */
+$this->registerCss(<<<CSS
+ 
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a {
+    margin: 0;
+    padding: 10px 10px
+}
+
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a>div>img {
+    margin: auto 10px auto auto;
+    width: 40px;
+    height: 40px
+}
+
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a>h4 {
+    padding: 0;
+    color: #444444;
+    font-size: 15px;
+    position: relative
+}
+
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a>h4>small {
+    color: #999999;
+    font-size: 10px;
+    position: absolute;
+    top: 0;
+    right: 0
+}
+
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a>p {
+    font-size: 12px;
+    color: #888888
+}
+
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a:before,
+.navbar-nav>.reminders>.dropdown-menu>li .menu>li>a:after {
+    content: " ";
+    display: table
+}
+
+.navbar-nav>.messages-menu>.dropdown-menu>li .menu>li>a:after {
+    clear: both
+}
+CSS
+
+);
 ?>
 <!-- Notifications Menu -->
-<li class="dropdown notifications-menu">
+<li class="dropdown notifications-menu reminders">
     <!-- Menu toggle button -->
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         <i class="fa fa-bell-o"></i>
@@ -21,10 +66,20 @@ use yii\helpers\Html;
                     <li>
                         <a href="#">
                             <h4>
-                                Support Team
-                                <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                                <?= Yii::t('hipanel/reminder', "{0} ID #{1}", ['Ticket', $reminder->object_id]) ?>
+                                <small><?= Yii::t('hipanel/reminder', 'Next time') ?>: <?= $reminder->next_time ?></small>
                             </h4>
-                            <p>Why not buy a new awesome theme?</p>
+                            <p>
+                                <?= \hipanel\helpers\StringHelper::truncateWords($reminder->message, 3) ?>
+                            </p>
+                            <small>
+                                <?= Yii::t('hipanel/reminder', 'Remine in') ?>:
+                                <button class="btn btn-xs btn-default">15m</button>
+                                <button class="btn btn-xs btn-default">30m</button>
+                                <button class="btn btn-xs btn-default">1h</button>
+                                <button class="btn btn-xs btn-default">12h</button>
+                                <button class="btn btn-xs btn-default">1d</button>
+                            </small>
                         </a>
                     </li>
                 <?php endforeach; ?>
