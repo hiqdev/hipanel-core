446d2c3d7025785815a8738c1003a7d2491388c9
tofid <andreyklochok@gmail.com>
2016-08-02 15:42:17 +0300

    Added new Datetimepicker

---
+++ src/assets/BootstrapDatetimepickerAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hipanel\assets;
+
+use yii\web\AssetBundle;
+
+class BootstrapDatetimepickerAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+
+    public $js = [
+        '',
+    ];
+}
