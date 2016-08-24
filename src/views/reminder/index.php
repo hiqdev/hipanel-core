1516adda1aa50b556cf34bcff5e313bde72e3740
Andrii Vasyliev <sol@hiqdev.com>
2016-08-24 07:58:26 +0000

    redone subtitle to original Yii style

--- src/views/reminder/index.php
+++ src/views/reminder/index.php
@@ -2,13 +2,13 @@
 
 use hipanel\grid\ReminderGridView;
 use hipanel\modules\stock\grid\PartGridView;
-use hipanel\widgets\IndexLayoutSwitcher;
 use hipanel\widgets\IndexPage;
 use hipanel\widgets\Pjax;
 
 $this->title = Yii::t('hipanel/reminder', 'Reminders');
-$this->subtitle = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
+$this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
 $this->params['breadcrumbs'][] = $this->title;
+
 $representation = Yii::$app->request->get('representation');
 
 ?>
@@ -19,41 +19,38 @@ $representation = Yii::$app->request->get('representation');
     'dataProvider' => $dataProvider,
     'searchView' => '@hipanel/views/reminder/_search',
 ]) ?>
-<?php $page->setSearchFormData(compact([])) ?>
+    <?php $page->setSearchFormData(compact([])) ?>
 
-<?php $page->beginContent('show-actions') ?>
-    <?= IndexLayoutSwitcher::widget() ?>
-    <?= $page->renderSorter([
-        'attributes' => [
-            'id',
-        ],
-    ]) ?>
-    <?= $page->renderPerPage() ?>
-<?php $page->endContent() ?>
+    <?php $page->beginContent('show-actions') ?>
+        <?= $page->renderLayoutSwitcher() ?>
+        <?= $page->renderSorter([
+            'attributes' => [
+                'id',
+            ],
+        ]) ?>
+        <?= $page->renderPerPage() ?>
+    <?php $page->endContent() ?>
 
-<?php $page->beginContent('bulk-actions') ?>
-    <?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/delete', 'danger') ?>
-<?php $page->endContent() ?>
+    <?php $page->beginContent('bulk-actions') ?>
+        <?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/delete', 'danger') ?>
+    <?php $page->endContent() ?>
 
-<?php $page->beginContent('table') ?>
-    <?php $page->beginBulkForm() ?>
-    <?= ReminderGridView::widget([
-        'boxed' => false,
-        'dataProvider' => $dataProvider,
-        'filterModel' => $model,
-        'tableOptions' => [
-            'class' => 'table table-striped table-bordered table-condensed'
-        ],
-        'columns' => [
-            'checkbox',
-
-            'periodicity',
-            'next_time',
-
-            'actions',
-        ],
-    ]) ?>
-    <?php $page->endBulkForm() ?>
-<?php $page->endContent() ?>
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+        <?= ReminderGridView::widget([
+            'boxed' => false,
+            'dataProvider' => $dataProvider,
+            'filterModel' => $model,
+            'tableOptions' => [
+                'class' => 'table table-striped table-bordered table-condensed'
+            ],
+            'columns' => [
+                'checkbox',
+                'periodicity', 'next_time',
+                'actions',
+            ],
+        ]) ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
 <?php $page->end() ?>
 <?php Pjax::end() ?>
