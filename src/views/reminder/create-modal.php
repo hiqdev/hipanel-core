79438befc2b42b1cc7d683a366172e2db999566f
tofid <andreyklochok@gmail.com>
2016-09-22 13:45:54 +0300

    Removed reminders files

--- src/views/reminder/create-modal.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var \hipanel\models\Reminder $model
- */
-// Set client offset
-$this->registerJs('$("#reminder-offset").val(moment().utcOffset())');
-?>
-
-<?= $this->render('_form', compact(['model', 'periodicityOptions'])); ?>
