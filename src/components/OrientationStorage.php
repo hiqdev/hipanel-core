d65ae39deb3ec7d6e7b336bf81c6a76db06013ab
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-10-19 15:32:09 +0300

    Implemented OrientationStorage using SettingsStorage

--- src/components/OrientationStorage.php
+++ src/components/OrientationStorage.php
@@ -14,54 +14,119 @@ namespace hipanel\components;
 use Yii;
 use yii\base\Component;
 
+/**
+ * Class OrientationStorage
+ *
+ * @package hipanel\components
+ */
 class OrientationStorage extends Component
 {
     const ORIENTATION_HORIZONTAL = 'horizontal';
     const ORIENTATION_VERTICAL = 'vertical';
 
-    public $sessionKey = 'orientationStorage';
-
     public $defaultOrientation = self::ORIENTATION_HORIZONTAL;
+
+    /**
+     * @var string the cache key that will be used to cache storage values
+     */
+    public $cacheKey;
+
+    /**
+     * @var string
+     */
+    public $settingsStorageKey = 'orientations';
 
     /**
      * @var array
      */
-    protected $storage = [];
+    protected $storage;
 
     /**
-     * @var string session component name
+     * @inheritdoc
      */
-    public $session = 'session';
-
-    protected function getSession()
+    public function init()
     {
-        return Yii::$app->getSession();
-    }
-
-    protected function getStorage()
-    {
-        $session = $this->getSession();
-
-        if ($session->has($this->sessionKey)) {
-            $this->storage = $session->get($this->sessionKey);
+        if (!isset($this->cacheKey)) {
+            $this->cacheKey = ['orientationStorage', Yii::$app->user->id];
         }
     }
 
-    protected function saveStorage()
+    /**
+     * @return \yii\caching\Cache
+     */
+    protected function getCache()
     {
-        $session = $this->getSession();
-        $session->set($this->sessionKey, $this->storage);
+        return Yii::$app->getCache();
     }
 
+    /**
+     * @return SettingsStorageInterface
+     */
+    protected function getSettingsStorage()
+    {
+        return Yii::$app->get('settingsStorage');
+    }
+
+    /**
+     * Ensures that [[storage]] contains actual orientations storage
+     *
+     * @return array
+     */
+    private function ensureStorage()
+    {
+        if (!isset($this->storage)) {
+            $this->storage = $this->getCache()->get($this->cacheKey);
+
+            if ($this->storage === false) {
+                $this->storage = $this->getSettingsStorage()->getBounded($this->settingsStorageKey);
+                $this->cache();
+            }
+        }
+
+        return $this->storage;
+    }
+
+    /**
+     * Saves current [[storage]]
+     */
+    protected function saveStorage()
+    {
+        $this->getSettingsStorage()->setBounded($this->settingsStorageKey, $this->storage);
+        $this->cache();
+    }
+
+    /**
+     * Caches current [[storage]]
+     */
+    protected function cache()
+    {
+        $this->getCache()->set($this->cacheKey, $this->storage, 86400); // 1 day
+    }
+
+    /**
+     * Sets orientation for the $route
+     *
+     * @param string $route
+     * @param string $orientation
+     */
     public function set($route, $orientation)
     {
+        $this->ensureStorage();
+
         $this->storage[$route] = $orientation;
         $this->saveStorage();
     }
 
+    /**
+     * Gets orientation for the $route
+     *
+     * @param $route
+     * @return string
+     */
     public function get($route)
     {
-        $this->getStorage();
+        $this->ensureStorage();
+
         return isset($this->storage[$route]) ? $this->storage[$route] : $this->defaultOrientation;
     }
 }
