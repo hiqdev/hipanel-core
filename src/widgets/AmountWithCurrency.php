a410c5d96bfc49611bc44135f5cd007640c690d2
tofid <andreyklochok@gmail.com>
2016-11-02 18:53:05 +0200

    Fixed widget validation if price and currency is required

--- src/widgets/AmountWithCurrency.php
+++ src/widgets/AmountWithCurrency.php
@@ -13,80 +13,36 @@ namespace hipanel\widgets;
 
 class AmountWithCurrency extends \yii\base\Widget
 {
-    const INPUT_TYPE_TEXT = 'text';
-    const INPUT_TYPE_NUMBER = 'number';
+    static public $widgetClass = 'amount-with-currency-widget';
 
-    /**
-     * @var
-     */
+    public $currencyAttributeName;
+    public $currencyAttributeOptions;
     public $model;
-
-    /**
-     * @var
-     */
     public $form;
-
-    /**
-     * @var
-     */
     public $attribute;
-
-    /**
-     * @var
-     */
-    public $inputOptions = [];
-
-    /**
-     * @var
-     */
-    public $inputAttributeType;
-
-    /**
-     * @var
-     */
-    public $selectAttribute;
-
-    /**
-     * @var
-     */
-    public $selectAttributeValue;
-
-    /**
-     * @var array
-     */
-    public $selectAttributeOptions = [];
-
-    public function init()
-    {
-        if ($this->inputAttributeType === null) {
-            $this->inputAttributeType = self::INPUT_TYPE_TEXT;
-        }
-    }
 
     public function run()
     {
         $this->initClientScript();
 
         return $this->render((new \ReflectionClass($this))->getShortName(), [
+            'form' => $this->form,
             'model' => $this->model,
-
+            'currencyAttributeName' => $this->currencyAttributeName,
+            'currencyAttributeOptions' => $this->currencyAttributeOptions,
             'attribute' => $this->attribute,
-            'inputAttributeType' => $this->inputAttributeType,
-            'inputOptions' => $this->inputOptions,
-
-            'selectAttribute' => $this->selectAttribute,
-            'selectAttributeOptions' => $this->selectAttributeOptions,
-            'selectAttributeValue' => $this->selectAttributeValue,
+            'containerClass' => self::$widgetClass,
         ]);
     }
 
     public function initClientScript()
     {
-        $this->getView()->registerJs(<<<'JS'
-        $(document).on('click', '.amount-with-currency-widget a', function(e) {
-            var $item = $(this);
-            $item.parents('.amount-with-currency-widget').find('.iwd-label').text($item.data('label'));
-            $item.parents('.amount-with-currency-widget').find(':hidden').val($item.data('value'));
+        $widgetClass = self::$widgetClass;
+        $this->getView()->registerJs(<<<"JS"
+        $(document).on('click', '.{$widgetClass} a', function(e) {
+            var item = $(this);
+            item.parents('.amount-with-currency-widget').find('.iwd-label').text(item.data('label'));
+            item.parents('.amount-with-currency-widget').find(':hidden').val(item.data('value')).trigger('change');
         });
 JS
         );
--- src/widgets/views/AmountWithCurrency.php
+++ src/widgets/views/AmountWithCurrency.php
@@ -2,23 +2,24 @@
 
 use hipanel\helpers\StringHelper;
 use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
+use yii\bootstrap\Html;
 
 ?>
-<div class="input-group amount-with-currency-widget">
+<div class="input-group">
     <div class="input-group-btn">
         <button type="button" class="btn btn-default iwd-label dropdown-toggle" data-toggle="dropdown">
-            <?= StringHelper::getCurrencySymbol(Html::getAttributeValue($model, $selectAttribute)) ?>
+            <?= StringHelper::getCurrencySymbol(Html::getAttributeValue($model, $currencyAttributeName)) ?>
         </button>
         <button type=button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <span class="caret"></span> <span class="sr-only"><?= Yii::t('hipanel', 'Toggle dropdown') ?></span>
         </button>
         <ul class="dropdown-menu">
-            <?php foreach (ArrayHelper::remove($selectAttributeOptions, 'items', []) as $k => $v) : ?>
-                <li><?= Html::a(StringHelper::getCurrencySymbol($k), '#', ['data-value' => $k, 'data-label' => StringHelper::getCurrencySymbol($k)]) ?>
+            <?php foreach (ArrayHelper::remove($currencyAttributeOptions, 'items', []) as $k => $v) : ?>
+                <li>
+                    <?= Html::a(StringHelper::getCurrencySymbol($k), '#', ['data-value' => $k, 'data-label' => StringHelper::getCurrencySymbol($k)]) ?>
+                </li>
             <?php endforeach ?>
         </ul>
     </div>
-    <?= Html::activeInput($inputAttributeType, $model, $attribute, array_merge(['class' => 'form-control'], $inputOptions)) ?>
-    <?= Html::activeHiddenInput($model, $selectAttribute, array_merge(['class' => 'form-control'], $selectAttributeOptions)) ?>
+    <?= Html::activeInput('text', $model, $attribute, ['class' => 'form-control']) ?>
 </div>
