ee83ecc76b8f7b8a246a3484f88eb4f9ec7ea2df
tofid <andreyklochok@gmail.com>
2016-11-04 19:57:48 +0200

    Changed. Redefined render method to custom widget menu call

--- src/menus/SidebarMenu.php
+++ src/menus/SidebarMenu.php
@@ -4,4 +4,13 @@ namespace hipanel\menus;
 
 class SidebarMenu extends \hiqdev\menumanager\Menu
 {
+    public function render($options = [])
+    {
+        $hipanelOptions = [
+            'class' => \hipanel\widgets\Menu::class,
+            'options' => ['class' => 'sidebar-menu'],
+        ];
+
+        return parent::render(array_merge($hipanelOptions, $options));
+    }
 }
