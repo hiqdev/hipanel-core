2e24d6bc6b4123963720067001f9e5760f9bf1e7
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-12-02 16:56:41 +0200

    CheckCircle widget moved from domain module

---
+++ src/widgets/CheckCircle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace hipanel\modules\domain\widgets;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class CheckCircle extends Widget
+{
+    /**
+     * @var boolean
+     */
+    public $value;
+
+    public function run()
+    {
+        return Html::tag('i', null, [
+            'class' => 'fa fa-fw fa-lg fa-check-circle pull-right',
+            'style' =>  'color: #' . (boolval($this->value) ? '00a65a' : 'b3b3b3')
+        ]);
+    }
+}
