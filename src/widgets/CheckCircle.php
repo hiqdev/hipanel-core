8cc3c62ca528533bf3a8a7b380c37e902f3a7542
tofid <andreyklochok@gmail.com>
2016-12-05 14:36:50 +0200

    Added colors by levels, takes Verrification model instead boolen value

--- src/widgets/CheckCircle.php
+++ src/widgets/CheckCircle.php
@@ -2,21 +2,55 @@
 
 namespace hipanel\widgets;
 
+use hipanel\modules\client\models\Verification;
+use Yii;
+use yii\base\InvalidConfigException;
 use yii\base\Widget;
 use yii\helpers\Html;
 
 class CheckCircle extends Widget
 {
+    const COLOR_VERIFIED = '#00a65a';
+    const COLOR_CONFIRMED = '#54b5ff';
+    const COLOR_UNCONFIRMED = '#b3b3b3';
+
     /**
-     * @var boolean
+     * @var Verification
      */
-    public $value;
+    public $model;
+
+    public function init()
+    {
+        if (!$this->model instanceof Verification) {
+            throw new InvalidConfigException(Yii::t('hipanel', 'Attribute "model" must be instance of Verification object'));
+        }
+    }
 
     public function run()
     {
         return Html::tag('i', null, [
-            'class' => 'fa fa-fw fa-lg fa-check-circle pull-right',
-            'style' =>  'color: #' . (boolval($this->value) ? '00a65a' : 'b3b3b3')
+            'class' => 'fa fa-fw fa-lg fa-check-circle pull-right verification-tooltip',
+            'style' => 'color: ' . $this->getColor(),
+            'title' => $this->model->getLabels()[$this->model->level],
+            'data' => [
+                'toggle' => 'tooltip',
+                'trigger' => 'hover',
+            ]
         ]);
+    }
+
+    protected function getColor()
+    {
+        $color = self::COLOR_UNCONFIRMED;
+        switch ($this->model) {
+            case $this->model->isVerified():
+                $color = self::COLOR_VERIFIED;
+                break;
+            case $this->model->isConfirmed():
+                $color = self::COLOR_CONFIRMED;
+                break;
+        }
+
+        return $color;
     }
 }
