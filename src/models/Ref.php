be201c02c45f1de36df8c046513ab5527d837e09
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-12-13 17:24:08 +0200

    Added Ref::findCached method

--- src/models/Ref.php
+++ src/models/Ref.php
@@ -26,27 +26,45 @@ class Ref extends \hiqdev\hiart\ActiveRecord
         ];
     }
 
+    /**
+     * Finds models by specified $name and $options parameters,
+     * transforms them to key-value structure.
+     * Name: attribute `name`
+     * Value: attribute `label`
+     *
+     * @param string $name
+     * @param string $translate
+     * @param array $options
+     * @return array
+     */
     public static function getList($name, $translate = null, $options = [])
+    {
+        $models = static::findCached($name, $translate, $options);
+        $result = ArrayHelper::map($models, 'name', 'label');
+
+        return $result;
+    }
+
+    public static function findCached($name, $translate = null, $options = [])
     {
         if ($translate === null) {
             $translate = 'hipanel';
         }
-        $function = function ($model) use ($translate) {
-            /** @var self $model */
-            if ($translate !== false) {
-                return Yii::t($translate, $model->label);
-            }
 
-            return $model->label;
-        };
-        $data = Yii::$app->get('cache')->getTimeCached(3600, [$name, $options, $translate], function ($name, $options, $translate) {
+        $data = Yii::$app->get('cache')->getTimeCached(3600, [$name, $options], function ($name, $options) {
             $conditions = array_merge(['gtype' => $name], $options);
             $result = self::find()->where($conditions)->search();
 
             return $result;
         });
-        $result = ArrayHelper::map($data, 'name', $function);
 
-        return $result;
+        return array_map(function ($model) use ($translate) {
+            /** @var self $model */
+            if ($translate !== false) {
+                $model->label = Yii::t($translate, $model->label);
+            }
+
+            return $model;
+        }, $data);
     }
 }
