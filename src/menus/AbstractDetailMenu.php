53c1869f8fbd61e0f3e262c896218f27c5278b28
Andrii Vasyliev <sol@hiqdev.com>
2016-12-21 13:35:20 +0000

    + AbstractDetailMenu

---
+++ src/menus/AbstractDetailMenu.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hipanel\menus;
+
+use hiqdev\menumanager\widgets\DetailMenu;
+
+abstract class AbstractDetailMenu extends \hiqdev\menumanager\Menu
+{
+    public $options = [
+        'class' => 'nav',
+    ];
+}
