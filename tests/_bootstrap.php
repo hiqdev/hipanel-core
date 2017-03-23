7e41ab5fae18f5385eac48ab7ca60a1d7e9625cc
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-23 12:04:12 +0200

    Updated tests bootstrap to support PHPUnit 6

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,14 +1,29 @@
 <?php
-
-/*
- * HiPanel core package
+/**
+ * PHPUnit plugin for HiDev.
  *
- * @link      https://hipanel.com/
- * @package   hipanel-core
+ * @link      https://github.com/hiqdev/hidev-phpunit
+ * @package   hidev-phpunit
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 error_reporting(E_ALL & ~E_NOTICE);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
