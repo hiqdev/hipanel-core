2fd912b149275adcd24ed33df41d5f3e6c169205
tofid <andreyklochok@gmail.com>
2017-03-28 13:45:38 +0300

    Added BETA ribbon

---
+++ src/views/layouts/_after_header.php
@@ -0,0 +1,22 @@
+<?= $this->registerCss('
+body:after {
+    content: "beta";
+    position: fixed;
+    width: 46px;
+    height: 19px;
+    background: #cc006a;
+    top: 0px;
+    left: -13px;
+    text-align: center;
+    font-size: 8px;
+    font-family: sans-serif;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: #fff;
+    line-height: 22px;
+    -ms-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+    z-index: 9999;
+}
+') ?>
