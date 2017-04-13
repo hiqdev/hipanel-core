9d663d97c1b81c2da2e8eba921c0fe191db9092b
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-04-13 13:12:41 +0300

    Added AsyncLoader

---
+++ src/widgets/AsyncLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace hipanel\widgets;
+
+use yii\base\InvalidConfigException;
+use yii\base\Widget;
+use yii\helpers\Json;
+use yii\helpers\Url;
+
+class AsyncLoader extends Widget
+{
+    public $route;
+
+    public $containerSelector;
+
+    public function init()
+    {
+        parent::init();
+
+        if (!isset($this->route)) {
+            throw new InvalidConfigException('The "url" property is not set');
+        }
+
+        if (!isset($this->containerSelector)) {
+            throw new InvalidConfigException('The "containerSelector" property is not set');
+        }
+    }
+
+    public function run()
+    {
+        $url = Json::htmlEncode(Url::to($this->route));
+        $selector = Json::htmlEncode($this->containerSelector);
+
+        $this->view->registerJs("$.get($url, function (data) {
+            $($selector).replaceWith(data);
+        });");
+
+        return $this->renderOverlay();
+    }
+
+    private function renderOverlay()
+    {
+        return '<div class="overlay"><i class="fa fa-refresh fa-spin"></i></div>';
+    }
+}
