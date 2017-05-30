dd86b6bc1505124cacf959a90f35dea55ff7250f
tofid <andreyklochok@gmail.com>
2017-05-30 19:03:01 +0300

    Added the ability to add `mapOptions` params to Ref::getList()

--- src/models/Ref.php
+++ src/models/Ref.php
@@ -30,6 +30,9 @@ class Ref extends \hiqdev\hiart\ActiveRecord
      * transforms them to key-value structure.
      * Name: attribute `name`
      * Value: attribute `label`.
+     * You can change name of returning attributes `name` and `label`
+     * You should define `mapOptions` in `options` array
+     * and define keys `from` for Name and `to` for Value
      *
      * @param string $name
      * @param string $translate
@@ -39,9 +42,13 @@ class Ref extends \hiqdev\hiart\ActiveRecord
     public static function getList($name, $translate = null, $options = [])
     {
         $models = static::findCached($name, $translate, $options);
-        $result = ArrayHelper::map($models, 'name', 'label');
 
-        return $result;
+        $mapOptions = ArrayHelper::remove($options, 'mapOptions');
+        $from = ArrayHelper::remove($mapOptions, 'from', 'name');
+        $to = ArrayHelper::remove($mapOptions, 'to', 'label');
+        $group = ArrayHelper::remove($mapOptions, 'group', null);
+
+        return ArrayHelper::map($models, $from, $to, $group);
     }
 
     public static function findCached($name, $translate = null, $options = [])
