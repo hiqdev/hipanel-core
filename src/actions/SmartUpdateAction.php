hiqdev/hipanel-core

commit 3f95ae2680282d649363a86d0c8e64b428d39d10

Improved code in SmartUpdateAction::getDefaultRules()

src/actions/SmartUpdateAction.php
@@ -159,11 +159,7 @@ class SmartUpdateAction extends SwitchAction
                             $model->scenario = $this->scenario;
                             foreach ($this->collection->models as $payload) {
                                 if ($payload->id === $model->id) {
-                                    foreach ($payload as $attribute => $value) {
-                                        if ($value) {
-                                            $model->{$attribute} = $value;
-                                        }
-                                    }
+                                    $model->setAttributes(array_filter($payload->getAttributes()));
                                 }
                             }
                         }

commit 06ad80f7955b88e63530bdf6355fac4b9609ef9d

Fixed regression intoruduced by previous commit (3f95ae2)
Originally reproduced on server/bulk-sale modal form: in case saving fails, `loadModels()` call
tries to get IDs from request by checking `id` and `selection` params. Both of them are empty,
so exception is thrown.

This commit fixes regression by falling back to simpler behavior in case of exception

src/actions/SmartUpdateAction.php
@@ -154,14 +154,19 @@ class SmartUpdateAction extends SwitchAction
                     'view'   => $this->view,
                     'data'   => $this->data,
                     'params' => function ($action) {
-                        $models = $this->fetchModels();
-                        foreach ($models as $model) {
-                            $model->scenario = $this->scenario;
-                            foreach ($this->collection->models as $payload) {
-                                if ($payload->id === $model->id) {
-                                    $model->setAttributes(array_filter($payload->getAttributes()));
+                        try {
+                            $models = $this->fetchModels();
+
+                            foreach ($models as $model) {
+                                $model->scenario = $this->scenario;
+                                foreach ($this->collection->models as $payload) {
+                                    if ($payload->id === $model->id) {
+                                        $model->setAttributes(array_filter($payload->getAttributes()));
+                                    }
                                 }
                             }
+                        } catch (BadRequestHttpException $e) {
+                            $models = $this->collection->models;
                         }
 
                         return [