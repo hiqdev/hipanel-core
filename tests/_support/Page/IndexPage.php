2c8b15e4aca1100e030fa8855cfe489551bb6fb1
pkolomiy <pkolomiy@gmail.com>
2018-08-02 16:41:35 +0300

    Added ability to change bulk view in IndexPage->containsColumns

--- tests/_support/Page/IndexPage.php
+++ tests/_support/Page/IndexPage.php
@@ -57,13 +57,19 @@ class IndexPage extends Authenticated
     }
 
     /**
-     * @param string $formId
      * @param string[] $columnNames array of column names
+     * @param string|null $view bulk view name
      */
-    public function containsColumns(array $columnNames): void
+    public function containsColumns(array $columnNames, $view = null): void
     {
         $I = $this->tester;
         $formId = $I->grabAttributeFrom("//form[contains(@id, 'bulk') and contains(@id, 'search')]", 'id');
+
+        if ($view) {
+            $I->click("//button[contains(text(), 'View:')]");
+            $I->click("//ul/li/a[contains(text(), '$view')]");
+            $I->waitForJS("return $.active == 0;", 15);
+        }
 
         foreach ($columnNames as $column) {
             $I->see($column, "//form[@id='$formId']//table/thead/tr/th");
