f0f94475669a7b616fa818bf560c1b9f13fcdebf
Andrey Klochok <andreyklochok@gmail.com>
2018-08-02 13:58:40 +0300

    Added AjaxModalDecorator widget

---
+++ src/widgets/AjaxModalDecorator.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace hipanel\widgets;
+
+use hipanel\helpers\ArrayHelper;
+use ReflectionClass;
+use yii\base\Widget;
+use yii\web\View;
+
+/**
+ * Class AjaxModalDecorator render AjaxModal markup to the end of body and render toggle button in any
+ * place of html with additional toggle button template.
+ */
+class AjaxModalDecorator extends Widget
+{
+    /**
+     * @var array
+     */
+    public $ajaxModalOptions;
+
+    /**
+     * @var string the template used to render a toggle button.
+     */
+    public $toggleButtonTemplate = "{toggleButton}";
+
+    /**
+     * @var array
+     */
+    private $toggleButtonOptions;
+
+    public function init()
+    {
+        $this->toggleButtonOptions = ArrayHelper::remove($this->ajaxModalOptions, 'toggleButton');
+        $this->ajaxModalOptions['toggleButton'] = false;
+        $this->view->on(View::EVENT_END_BODY, function ($event) {
+            echo AjaxModal::widget($this->ajaxModalOptions);
+        });
+    }
+
+    public function run()
+    {
+        $id = $this->ajaxModalOptions['id'];
+        $toggleButton = function () use ($id) {
+            $this->options['id'] = $id;
+            $this->initOptions();
+
+            return $this->renderToggleButton();
+        };
+        $obj = (new ReflectionClass(AjaxModal::class))->newInstanceWithoutConstructor();
+        $obj->toggleButton = $this->toggleButtonOptions;
+
+        $button = $toggleButton->call($obj);
+        if ($button) {
+            $button = strtr($this->toggleButtonTemplate, [
+                '{toggleButton}' => $button,
+            ]);
+        }
+
+        return $button;
+    }
+}
