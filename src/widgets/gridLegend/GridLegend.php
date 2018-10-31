7ecd1c046e7ee6c762ee42894c1e546a8698ef2e
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-10-31 16:01:05 +0200

    Code style fixes

--- src/widgets/gridLegend/GridLegend.php
+++ src/widgets/gridLegend/GridLegend.php
@@ -14,7 +14,6 @@ class GridLegend extends Widget
 
     /**
      * @param GridLegendInterface $legendItem
-     * @param array $config
      * @return self
      */
     public static function create(GridLegendInterface $legendItem)
@@ -25,7 +24,7 @@ class GridLegend extends Widget
     public function gridRowOptions()
     {
         foreach ($this->legendItem->items() as $item) {
-            if (!isset($item['columns']) && isset($item['rule']) && boolval($item['rule']) === true) {
+            if (!isset($item['columns']) && isset($item['rule']) && (bool)$item['rule'] === true) {
                 return ['style' => "background-color: {$this->getColor($item)} !important;"];
             }
         }
@@ -36,7 +35,7 @@ class GridLegend extends Widget
     public function gridColumnOptions($column)
     {
         foreach ($this->legendItem->items() as $item) {
-            if (isset($item['columns']) && in_array($column, $item['columns'], true) && boolval($item['rule']) === true) {
+            if (isset($item['columns']) && \in_array($column, $item['columns'], true) && (bool)$item['rule'] === true) {
                 return ['style' => "background-color: {$this->getColor($item)} !important;"];
             }
         }
@@ -46,12 +45,12 @@ class GridLegend extends Widget
 
     public function getColor($item)
     {
-        return isset($item['color']) ? $item['color'] : 'transparent';
+        return $item['color'] ?? 'transparent';
     }
 
     public function getLabel($item)
     {
-        return isset($item['label']) ? (is_array($item['label']) ? Yii::t($item['label'][0], $item['label'][1]) : $item['label']) : Yii::t('hipanel', 'Empty');
+        return isset($item['label']) ? (\is_array($item['label']) ? Yii::t($item['label'][0], $item['label'][1]) : $item['label']) : Yii::t('hipanel', 'Empty');
     }
 
     public function run()
