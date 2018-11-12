9bcebd90902e7612295e9a8df979bf23bfb16953
Artem Pakhomov <setnemo@gmail.com>
2018-11-12 12:31:33 +0200

    Add dashboard helper (#70)
    * Added helper for dashboard
    
    * Fixed namespaces
    
    * Added end of line
    
    * fixed arguments

---
+++ tests/_support/DashboardHelper/DashboardSearchBox.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace hipanel\tests\_support\DashboardHelper;
+
+use hipanel\tests\_support\AcceptanceTester;
+
+class DashboardSearchBox
+{
+    /**
+     * @var AcceptanceTester
+     */
+    protected $tester;
+
+    /**
+     * DashboardSearchBox constructor.
+     * @param AcceptanceTester $tester
+     */
+    public function __construct(AcceptanceTester $tester)
+    {
+        $this->tester = $tester;
+    }
+
+    /**
+     * Ensure is search box contains on testing page
+     * @param string $formAction
+     * @param string $inputName
+     * @param string $typeInput
+     */
+    public function ensureSearchBoxContains(
+        string $formAction,
+        string $inputName,
+        string $typeInput
+    ): void
+    {
+        $I = $this->tester;
+        $formActionXpath = "//form[contains(@action,'" . $formAction . "')]";
+        $input = $formActionXpath . "//". $typeInput . "[contains(@name, '$inputName')]";
+
+        $I->seeInCurrentUrl('/dashboard/dashboard');
+        $I->seeElement($formAction);
+        $I->seeElement($input);
+    }
+}
