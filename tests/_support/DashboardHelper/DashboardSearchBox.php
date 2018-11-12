42bfa2711d4371449291f96629a010848f9c7b9f
Artem Pakhomov <setnemo@gmail.com>
2018-11-12 10:19:26 +0200

    Added helper for dashboard

---
+++ tests/_support/DashboardHelper/DashboardSearchBox.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace hipanel\tests\_support\DashboardHelpers;
+
+use hipanel\tests\_support\AcceptanceTester;
+
+class DashboardSearchBox
+{
+    /**
+     * @var AcceptanceTester
+     */
+    protected $tester;
+
+    /**
+     * @var array $data
+     */
+    protected $data;
+
+    /**
+     * DashboardSearchBox constructor.
+     * @param AcceptanceTester $tester
+     * @param array $testData for protected $data
+     *  [
+     *     'formAction' => '/finance/tariff/index',
+     *     'inputName'  => 'TariffSearch[tariff_like]',
+     *     'typeInput'  => 'input',
+     *  ]
+     */
+    public function __construct(AcceptanceTester $tester, array $testData)
+    {
+        $this->tester = $tester;
+        $this->data = $testData;
+    }
+
+    /**
+     * Ensure is search box contains on testing page
+     *
+     */
+    public function ensureSearchBoxContains(): void
+    {
+        $I = $this->tester;
+        $formAction = "//form[contains(@action,'" . $this->data['formAction'] . "')]";
+        $input = $formAction . "//". $this->data['typeInput'] . "[contains(@name, '" . $this->data['inputName'] . "')]";
+
+        $I->seeInCurrentUrl('/dashboard/dashboard');
+        $I->seeElement($formAction);
+        $I->seeElement($input);
+    }
+}
