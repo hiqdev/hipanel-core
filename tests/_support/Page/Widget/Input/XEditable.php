5ee24ad2c07c38aa06e3dc18f60f840492773f25
Artem Pakhomov <setnemo@gmail.com>
2018-11-16 23:02:21 +0200

    Added extends TestableInput

--- tests/_support/Page/Widget/Input/XEditable.php
+++ tests/_support/Page/Widget/Input/XEditable.php
@@ -2,41 +2,49 @@
 
 namespace hipanel\tests\_support\Page\Widget\Input;
 
-use hipanel\tests\_support\AcceptanceTester;
-
 /**
  * Class XEditable
  *
  * Represent XEditable input element.
  * @package hipanel\tests\_support\Page\Widget\Input
  */
-class XEditable
+class XEditable extends TestableInput
 {
-    private $tester;
+    private $row;
 
-    public function __construct(AcceptanceTester $tester)
+    /**
+     * @param mixed $row
+     */
+    public function setRow($row): void
     {
-        $this->tester = $tester;
+        $this->row = $row;
     }
 
     /**
-     * @return int
+     * @param string $value
      */
-    public function getRowsInTableForFill(): int
+    public function setValue(string $value): void
     {
-        return count($this->tester->grabMultiple("//a[contains(@class, 'editable')]"));
+        $this->tester->click("div.price-item:nth-child($this->row) a.editable");
+        (new Input($this->tester, "div.price-item:nth-child($this->row) div.editable-input input"))
+            ->setValue($value);
+        $this->tester->click("div.price-item:nth-child($this->row) button[type=submit]");
     }
 
     /**
-     * @param string $note
-     * @param int $row
+     * @return string
      */
-    public function fillNoteWithoutAjax(string $note, int $row): void
+    protected function getSearchSelector(): string
     {
-        $this->tester->click("div.price-item:nth-child($row) a.editable");
-        (new Input($this->tester, "div.price-item:nth-child($row) div.editable-input input"))
-            ->setValue($note);
-        $this->tester->click("div.price-item:nth-child($row) button[type=submit]");
+        // TODO: Implement getSearchSelector() method.
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFilterSelector(): string
+    {
+        // TODO: Implement getFilterSelector() method.
     }
 }
 
