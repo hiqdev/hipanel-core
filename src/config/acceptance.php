46247bdb0222bc64f44fa7219324f83cceb639fe
Andrii Vasyliev <sol@hiqdev.com>
2018-12-20 15:36:50 +0200

    Added `acceptance` config

---
+++ src/config/acceptance.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'PARAMS_LOCATION' => dirname(__DIR__) . '/vendor/hiqdev/composer-config-plugin-output/acceptance.php',
+    'YII2_CONFIG_LOCATION' => dirname(__DIR__) . '/tests/acceptance/config/suite.php',
+
+    'COMMON_SUITE_LOCATION' => dirname(__DIR__) . '/tests/acceptance.suite.yml',
+    'COMMON_TESTS_LOCATION' => dirname(__DIR__) . '/tests',
+
+    'URL' => $params['hipanel.url'],
+    'BROWSER' => 'chrome',
+    'SELENIUM_HOST' => $params['tests.acceptance.selenium.host'],
+
+    'client' => [
+        'id' => '360113632',
+        'login' => 'hipanel_test_user@hiqdev.com',
+        'password' => 'random',
+    ],
+
+    'seller' => [
+        'id' => '360113767',
+        'login' => 'hipanel_test_reseller@hiqdev.com',
+        'password' => 'random',
+    ],
+
+    'manager' => [
+        'id' => '360087881',
+        'login' => 'hipanel_test_manager@hiqdev.com',
+        'password' => 'random',
+    ],
+
+    'admin' => [
+        'id' => '360113706',
+        'login' => 'hipanel_test_admin@hiqdev.com',
+        'password' => 'random',
+    ],
+];
