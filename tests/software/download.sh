0e6916afa71562204792549d50482689610ae154
Andrii Vasyliev <sol@hiqdev.com>
2019-01-14 15:08:29 +0200

    Updated versions in `download.sh`

--- tests/software/download.sh
+++ tests/software/download.sh
@@ -4,15 +4,14 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-wget -c http://selenium-release.storage.googleapis.com/3.1/selenium-server-standalone-3.1.0.jar
-wget -c https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
+wget -c https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
+wget -c https://chromedriver.storage.googleapis.com/2.45/chromedriver_linux64.zip
 unzip -f chromedriver_linux64.zip
-rm chromedriver_linux64.zip
 
 cat <<EOF > run.sh
 #!/bin/bash
 
-java -jar -Dwebdriver.chrome.driver=chromedriver selenium-server-standalone-3.1.0.jar
+java -jar -Dwebdriver.chrome.driver=chromedriver selenium-server-standalone-3.141.59.jar
 EOF
 
 chmod u+x run.sh
