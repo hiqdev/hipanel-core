15032e51a3a5cc0ea80162e41431e92ec1dda363
Andrey Klochok <andreyklochok@gmail.com>
2019-11-08 19:03:08 +0200

    Added banners to `_after_header` (#123)

--- src/views/layouts/_after_header.php
+++ src/views/layouts/_after_header.php
@@ -1,1 +1,14 @@
+<?php
+
+use hipanel\widgets\AdBanner;
+use hipanel\widgets\SidebarAdBanner;
+
+$this->beginBlock('ad-banner.dashboard');
+echo AdBanner::widget();
+$this->endBlock();
+
+$this->beginBlock('ad-banner.sidebar');
+echo SidebarAdBanner::widget();
+$this->endBlock();
+?>
 
