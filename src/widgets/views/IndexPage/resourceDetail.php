7e3060df302ad194ce915dce18f3d58e40687c66
Andrey Klochok <andreyklochok@gmail.com>
2020-10-20 17:03:59 +0300

    Add new IndexPage view

---
+++ src/widgets/views/IndexPage/resourceDetail.php
@@ -0,0 +1,16 @@
+<div class="box box-widget">
+    <div class="box-header">
+        <h4 class="box-title">
+            <?= $this->context->renderContent('title') ?>
+        </h4>
+        <div class="box-tools pull-right">
+            <?= $this->context->renderContent('actions') ?>
+        </div>
+    </div>
+    <div class="box-body no-padding">
+        <div class="overlay" style="display: none;">
+            <i class="fa fa-refresh fa-spin"></i>
+        </div>
+        <?= $this->context->renderContent('table') ?>
+    </div>
+</div>
