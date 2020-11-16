0289b23681c18bb63b3ad02b5d0cc26b04704ed2
Andrey Klochok <andreyklochok@gmail.com>
2020-11-16 13:03:43 +0200

    Add set `url` ability for `HookTrait`

--- src/widgets/HookTrait.php
+++ src/widgets/HookTrait.php
@@ -3,17 +3,21 @@
 namespace hipanel\widgets;
 
 use hipanel\actions\Action;
+use yii\helpers\Json;
 use yii\web\View;
 
 trait HookTrait
 {
+    public ?string $url = null;
+
     public function registerJsHook(string $reqHeaderParamName): void
     {
         $id = $this->getId();
         $headerName = Action::EXPECTED_AJAX_RESPONSE_HEADER_NAME;
+        $url = $this->url ? Json::encode($this->url) : 'document.URL';
         $this->view->registerJs("$.ajax({
            type: 'GET',
-           url: document.URL,
+           url: {$url},
            beforeSend: xhr => {
              xhr.setRequestHeader('{$headerName}', '{$reqHeaderParamName}');
            },
