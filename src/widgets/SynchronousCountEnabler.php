2299e3f4875e07a2d77236771ded7426a0da116d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2020-12-15 13:49:39 +0200

    Refactored SynchronousCountEnabler

--- src/widgets/SynchronousCountEnabler.php
+++ src/widgets/SynchronousCountEnabler.php
@@ -1,33 +1,66 @@
 <?php
+declare(strict_types=1);
 
 namespace hipanel\widgets;
 
+use Closure;
 use hiqdev\hiart\ActiveDataProvider;
 use Yii;
 use yii\base\DynamicModel;
-use yii\base\Widget;
 use yii\grid\GridView;
 
-class SynchronousCountEnabler extends Widget
+final class SynchronousCountEnabler
 {
-    public ActiveDataProvider $dataProvider;
+    private bool $loadModels = true;
+    private ActiveDataProvider $dataProvider;
+    private Closure $renderContent;
 
-    public $content;
-
-    public function run()
+    public function __construct(ActiveDataProvider $dataProvider, Closure $renderContent)
     {
-        $this->dataProvider->enableSynchronousCount();
+        $dataProvider->enableSynchronousCount();
 
-        $dataProvider = clone $this->dataProvider;
-        $dataProvider->setModels([new DynamicModel()]);
-        $dataProvider->setKeys([null]);
-        $dataProvider->setPagination(['totalCount' => $dataProvider->getTotalCount()]);
+        $this->dataProvider = clone $dataProvider;
+        $this->renderContent = $renderContent;
+    }
+
+    public function __invoke(): string
+    {
+        $dataProvider = $this->dataProvider;
+
+        if (! $this->loadModels) {
+            $pageSize = $dataProvider->pagination->pageSize;
+            $dataProvider->setModels(array_pad([], $pageSize, new DynamicModel()));
+            $dataProvider->setKeys(array_pad([], $pageSize, null));
+        }
+        $dataProvider->pagination->totalCount = $this->getCachedTotalCount($dataProvider);
 
         $grid = Yii::createObject([
             'class' => GridView::class,
             'dataProvider' => $dataProvider,
         ]);
 
-        return call_user_func($this->content, $grid);
+        return call_user_func($this->renderContent, $grid);
+    }
+
+    private function getCachedTotalCount(ActiveDataProvider $dataProvider): int
+    {
+        return Yii::$app->cache->getOrSet(
+            [
+                Yii::$app->user->getId(),
+                $dataProvider->query->modelClass,
+                $dataProvider->query->where,
+                Yii::$app->request->getHostName(),
+                __METHOD__,
+            ],
+            fn(): int => $dataProvider->getTotalCount(),
+            5
+        );
+    }
+
+    public function preventModelsLoading(): self
+    {
+        $this->loadModels = false;
+
+        return $this;
     }
 }
