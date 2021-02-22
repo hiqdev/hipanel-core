50679b54e748b16cbb40b6731e77fea9b563cb72
Andrey Klochok <andreyklochok@gmail.com>
2021-02-22 19:15:34 +0200

    Improve `resourceDetail` IndexPage view

--- src/widgets/views/IndexPage/resourceDetail.php
+++ src/widgets/views/IndexPage/resourceDetail.php
@@ -1,10 +1,12 @@
 <div class="box box-widget">
-    <div class="box-header">
-        <h4 class="box-title">
-            <?= $this->context->renderContent('title') ?>
-        </h4>
-        <div class="box-tools pull-right">
-            <?= $this->context->renderContent('actions') ?>
+    <div class="box-header-wrapper">
+        <div class="box-header">
+            <h4 class="box-title">
+                <?= $this->context->renderContent('title') ?>
+            </h4>
+            <div class="box-tools pull-right">
+                <?= $this->context->renderContent('actions') ?>
+            </div>
         </div>
     </div>
     <div class="box-body no-padding">
