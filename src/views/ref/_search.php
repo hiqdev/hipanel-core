c2fc6b4f56e84a08ac4d06031022314137f1d4c4
strorch <smy980807@ukr.net>
2021-06-04 02:05:08 +0300

    created filters loading on page load

--- src/views/ref/_search.php
+++ src/views/ref/_search.php
@@ -6,6 +6,9 @@
  * @var \yii\web\View $this
  */
 
+use hipanel\models\Ref;
+use hipanel\models\RefSearch;
+
 $this->registerJs(<<<JS
 
 (() => {
@@ -38,7 +41,7 @@ $this->registerJs(<<<JS
             
             $('<div class="col-md-4 col-sm-6 col-xs-12 ">' +
                 '<div class="form-group field-refsearch-gtype" data-toggle="tooltip" data-title="Gtype" data-original-title="" title="" xpath="1">' +
-                    `<select id="refsearch-gtype" class="changeable" data-num="\${contextNumber + 1}">` +
+                    `<select id="refsearch-gtype" class="changeable" name="RefSearch[gtype]" data-num="\${contextNumber + 1}">` +
                         options.join() +
                     '</select>' +
                 '</div>' +
@@ -52,24 +55,42 @@ $this->registerJs(<<<JS
     $("#form-advancedsearch-ref-search").submit(function(event) {
         const maxFilter = parseInt(Math.max.apply(Math, $(`select[class="changeable"]`).map(function() { return $(this).data().num; })));
         
-        const keyToSave = $(`select[class="changeable"][data-num="\${maxFilter}"])`).children("option:selected").val() !== ""
+        const keyToSave = $(`select[class="changeable"][data-num="\${maxFilter}"]`).children("option:selected").val() !== ""
                             ? maxFilter
                             : maxFilter - 1;
         
-        $(`select[class="changeable"]:not([data-num="\${keyToSave}"])`).remove();
+        $(`select[class="changeable"]:not([data-num="\${keyToSave}"])`).parent().parent().remove();
         debugger;
     });
     
-    function renderElems() {
-    
-        $('.changeable').change(changeableHandler);
-    }
-    
-    renderElems();
+    $('.changeable').change(changeableHandler);
 })();
 
 JS
 );
 
+$request = Yii::$app->request->get();
+$gtype = $request['RefSearch']['gtype'] ?? RefSearch::DEFAULT_SEARCH_ATTRIBUTE;
+$refBuild = '';
+$gtypeParts = ['', ...explode(',', $gtype)];
+
 ?>
 
+<?php foreach ($gtypeParts as $key => $refPart): ?>
+    <?php
+        $refBuild = implode(',', array_filter([$refBuild, $refPart]));
+        $childs = array_keys(Ref::getList($refBuild));
+        $arrayRefs = array_combine($childs, $childs);
+        $search->model->gtype = $refBuild;
+    ?>
+    <div class="col-md-4 col-sm-6 col-xs-12 ">
+        <?= $search->field('gtype')->dropDownList($arrayRefs, [
+            'class' => 'changeable',
+            'data-num' => 1,
+            'prompt'    => Yii::t('hipanel', '----------'),
+            'options' => [
+                $arrayRefs[$gtypeParts[$key + 1]] ?? '' => ['selected' => true],
+            ],
+        ]) ?>
+    </div>
+<?php endforeach; ?>
