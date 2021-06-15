c13cf88b266255a7ccc292d0740dbf42dd59c1da
Drakorgaur <re.markofdark@gmail.com>
2021-06-15 08:29:23 +0300

    Added ClickLinkHelper

---
+++ tests/_support/Helper/ClickLinkHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace hipanel\tests\_support\Helper;
+
+class ClickLinkHelper extends \Codeception\Module
+{
+    /**
+     * @param $linkText
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function clickLink(string $linkText, string $xpathPrefix = ''): void
+    {
+        $I = $this->getModule('WebDriver');
+        $selector = $xpathPrefix . "//a[contains(text(), '$xpathPrefix')]";
+        $I->click($selector);
+    }
+}
