963dd368cccf6b0f9868bd85033baa7c239ce7e7
mark <re.markofdark@gmail.com>
2021-09-16 09:37:38 +0300

    minor

--- tests/_support/Page/Widget/Grid.php
+++ tests/_support/Page/Widget/Grid.php
@@ -132,7 +132,7 @@ class Grid
             $columnNumber++;
         }
 
-        throw new \Exception("failed detect column with name $columnName");
+        throw new \Exception("Failed detect column with name $columnName");
     }
 
     public function getRowNumberInColumnByValue(string $columnName, string $rowValue): int
@@ -148,7 +148,7 @@ class Grid
             $rowNumber++;
         }
 
-        throw new \Exception("failed detect row with name $rowValue");
+        throw new \Exception("Failed detect row with name $rowValue");
     }
 
     public function getRowNumberByNameFromSummary(string $rowName): int
