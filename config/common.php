fa6299dc052b8c9c6f7ec38b613f3be80d95bf0a
Andrey Klochok <andreyklochok@gmail.com>
2022-03-07 10:01:42 +0200

    Add fake event panel to prevent write the events to debug panel (#216)

--- config/common.php
+++ config/common.php
@@ -68,6 +68,13 @@ return [
                 'ru' => 'Русский',
             ],
         ],
+        'debug' => empty($params['debug.event.enable']) ? null : [
+            'panels' => [
+                'event' => [
+                    'class' => \hipanel\panels\FakeEventPanel::class,
+                ],
+            ],
+        ],
     ],
     'container' => [
         'singletons' => [
