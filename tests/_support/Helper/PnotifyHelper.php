a99fb8dab94b9671d5dcefd16323bc02df9cf828
gadezist <andrybitko@gmail.com>
2022-06-30 16:32:53 +0300

    HP-858 add waitForElement for fix CI tests in finance module

--- tests/_support/Helper/PnotifyHelper.php
+++ tests/_support/Helper/PnotifyHelper.php
@@ -14,11 +14,19 @@ use Codeception\Module\WebDriver;
 
 class PnotifyHelper extends \Codeception\Module
 {
-    public function closeNotification(string $text): void
+    public function closeNotification(string $text, ?string $textError = null): void
     {
         /** @var WebDriver $I */
         $I = $this->getModule('WebDriver');
-        $I->waitForElement('.ui-pnotify', 180);
+        $I->waitForElement('.ui-pnotify', 60);
+        if ($textError !== null) {
+            $grabTextNotification = $I->grabTextFrom('.ui-pnotify');
+            if (in_array($grabTextNotification, compact('text', 'textError'))) {
+                $text = $grabTextNotification;
+            } else {
+                $I->fail("Don't see '{$text}' or '{$textError}' on notification");
+            }
+        }
         $I->see($text, '.ui-pnotify');
         $I->moveMouseOver(['css' => '.ui-pnotify']);
         $I->wait(0.5);
