8dcd75a8e0c8f5648f859845c0902a45ad585c71
Yurii Myronchuk <bladeroot@hiqdev.com>
2022-08-03 17:52:15 +0000

    fix warnings

--- src/views/layouts/_userinfo.php
+++ src/views/layouts/_userinfo.php
@@ -9,43 +9,49 @@ use yii\helpers\Url;
  * @var Client $client
  */
 
-$client = Client::findOne(Yii::$app->user->identity->id);
-
-if ($client->balance > 0) {
-    $balanceColor = 'text-success';
-} elseif ($client->balance < 0) {
-    $balanceColor = 'text-danger';
-} else {
-    $balanceColor = 'text-muted';
-}
-
-$this->registerCss('
-.user-panel > .info { padding-top: 0; } 
-.user-panel > .info a:first-child { font-size: 16px; } 
-');
-$tooltip = [];
-if ($client->credit >= 0) {
-    $tooltip = [
-        'data' => [
-            'toggle' => 'tooltip',
-            'placement' => 'bottom',
-            'title' => Yii::t('adminlte', 'Credit: {credit}', [
-                'credit' => Yii::$app->formatter->asCurrency($client->credit, $client->currency)
-            ]),
-        ],
-    ];
+if (Yii::$app->user->identity !== null) {
+    $client = Client::findOne(Yii::$app->user->identity->id);
 }
 
 ?>
-<?= Html::beginTag('a', array_merge([
-    'href' => Yii::$app->user->can('deposit') ? Url::to('@pay/deposit') : '#',
-], $tooltip)) ?>
+<?php if ($client !== null) : ?>
+    <?php
+    if ($client->balance > 0) {
+        $balanceColor = 'text-success';
+    } elseif ($client->balance < 0) {
+        $balanceColor = 'text-danger';
+    } else {
+        $balanceColor = 'text-muted';
+    }
 
-<i class="fa fa-circle <?= $balanceColor ?>"></i>
-<?= Yii::t('adminlte', 'Balance: {balance}', [
-    'balance' => Yii::$app->formatter->asCurrency($client->balance, $client->currency),
-]) ?>
+    $this->registerCss('
+        .user-panel > .info { padding-top: 0; }
+        .user-panel > .info a:first-child { font-size: 16px; }
+    ');
+    $tooltip = [];
+    if ($client->credit >= 0) {
+        $tooltip = [
+            'data' => [
+                'toggle' => 'tooltip',
+                'placement' => 'bottom',
+                'title' => Yii::t('adminlte', 'Credit: {credit}', [
+                    'credit' => Yii::$app->formatter->asCurrency($client->credit, $client->currency)
+                ]),
+            ],
+        ];
+    }
 
-<?= Html::endTag('a') ?>
-<br/>
-<?= Html::a(Yii::t('hipanel', 'User profile'), ['/site/profile']) ?>
+    ?>
+    <?= Html::beginTag('a', array_merge([
+        'href' => Yii::$app->user->can('deposit') ? Url::to('@pay/deposit') : '#',
+    ], $tooltip)) ?>
+
+    <i class="fa fa-circle <?= $balanceColor ?>"></i>
+    <?= Yii::t('adminlte', 'Balance: {balance}', [
+        'balance' => Yii::$app->formatter->asCurrency($client->balance, $client->currency),
+    ]) ?>
+
+    <?= Html::endTag('a') ?>
+    <br/>
+    <?= Html::a(Yii::t('hipanel', 'User profile'), ['/site/profile']) ?>
+<?php endif ?>
