54cc4b0f043c2884041bd4d0e79ede782295ec13
Andrey Klochok <andreyklochok@gmail.com>
2022-08-17 15:57:57 +0300

    HP-934 tests & CI

--- tests/playwright/input/Select2.ts
+++ tests/playwright/input/Select2.ts
@@ -2,12 +2,10 @@ import { expect, Locator, Page } from "@playwright/test";
 
 export default class Select2 {
   private dropdownSearchField: Locator;
-  private searchResult: Locator;
   private combobox: Locator;
 
-  private constructor(page: Page, fieldId: string) {
-    this.dropdownSearchField = page.locator(`span.select2-search.select2-search--dropdown > input`);
-    this.searchResult = page.locator(".select2-container .select2-results__option--highlighted");
+  private constructor(private page: Page, fieldId: string) {
+    this.dropdownSearchField = page.locator(".select2-container--open input.select2-search__field");
     this.combobox = page.locator(`${fieldId} + .select2-container [role=combobox]`);
   }
 
@@ -18,8 +16,8 @@ export default class Select2 {
   async setValue(value: string) {
     await this.combobox.click();
     await this.dropdownSearchField.fill(value);
-    await expect(this.searchResult.first()).toContainText(value);
-    await this.searchResult.first().click();
+    await this.page.locator("ul.select2-results__options .loading-results").waitFor({ state: "hidden" });
+    await this.page.locator(`//ul[contains(@class, 'select2-results__options')]/li[contains(text(), '${value}')]`).click();
   }
 }
 
