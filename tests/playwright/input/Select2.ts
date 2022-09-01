f24058d9e6d379aee3603bb5e7e86ea601ccfbe2
gadezist <andrybitko@gmail.com>
2022-09-01 13:29:14 +0300

    HP-934 add some new function to base Playwright

--- tests/playwright/input/Select2.ts
+++ tests/playwright/input/Select2.ts
@@ -3,14 +3,21 @@ import { expect, Locator, Page } from "@playwright/test";
 export default class Select2 {
   private dropdownSearchField: Locator;
   private combobox: Locator;
+  private fieldId: string;
 
   private constructor(private page: Page, fieldId: string) {
     this.dropdownSearchField = page.locator(".select2-container--open input.select2-search__field");
     this.combobox = page.locator(`${fieldId} + .select2-container [role=combobox]`);
+    this.fieldId = fieldId;
   }
 
   static field(page: Page, fieldId: string): Select2 {
     return new Select2(page, fieldId);
+  }
+
+  static filterBy(page: Page, columnName: string) {
+    const fieldId = columnName.toLowerCase().substring(0, 5);
+    return this.field(page, `tr.filters select[id*=${fieldId}]`);
   }
 
   async setValue(value: string) {
