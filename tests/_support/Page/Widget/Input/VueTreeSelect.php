ed672976cab383ff38d4ee1d02e52603d5c1c8d4
Andrey Klochok <andreyklochok@gmail.com>
2022-12-16 13:44:46 +0200

    HP-1164: VueTreeSelect update

--- tests/_support/Page/Widget/Input/VueTreeSelect.php
+++ tests/_support/Page/Widget/Input/VueTreeSelect.php
@@ -13,11 +13,28 @@ class VueTreeSelect extends TestableInput
 
     protected function getFilterSelector(): string
     {
-        // TODO: Implement getFilterSelector() method.
+        return "//*[contains(@class, 'filters')]"
+            . "//div[contains(@class, 'vue-treeselect__placeholder')][contains(text(), '$this->title')]"
+            . "/following-sibling::div[contains(@class, 'vue-treeselect__input-container')]/input";
     }
 
     public function setValue(string $value): void
     {
-        // TODO: Implement setValue() method.
+        if (!empty($value)) {
+            $this->selectOption($value);
+        }
+        $this->removeOption($value);
+    }
+
+    private function selectOption(string $value): void
+    {
+        $this->tester->click($this->selector);
+        $this->tester->fillField($this->selector, $value);
+        $this->tester->click("//*[@class='vue-treeselect__label'][text()='$value']");
+    }
+
+    private function removeOption(?string $value): void
+    {
+        // TODO: Implement if $value empty then clear all, otherwise remove one option
     }
 }
