0749bdbb97bcb1f8461819e478e3fb62ac6a4a9a
gadezist <andrybitko@gmail.com>
2023-07-03 19:14:55 +0300

    HP-1439 Fix typo

--- src/components/I18N.php
+++ src/components/I18N.php
@@ -18,7 +18,10 @@ class I18N extends \yii\i18n\I18N
      */
     public function translate($category, $message, $params, $language)
     {
-        if (stripos($message ?? '', '{lang:') !== false) {
+        if (empty($message)) {
+            return '';
+        }
+        if (stripos($message, '{lang:') !== false) {
             $message = $this->removeLegacyLangTags($message);
         }
 
