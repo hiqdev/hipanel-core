28810ddced25b44243e1137cd1efb601baa5568f
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-06-05 09:26:41 +0300

    HP-1966: created DetailMenu for Playwright

---
+++ tests/playwright/helper/DetailMenu.ts
@@ -0,0 +1,21 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export default class DetailMenu {
+    public root: Locator;
+
+    constructor(private readonly page: Page) {
+        this.root = this.page.locator(".profile-usermenu .nav");
+    }
+
+    detailMenuItem(item: string): Locator {
+        return this.root.locator(`:scope a:text("${item}")`);
+    }
+
+    async clickDetailMenuItem(item: string) {
+        await this.detailMenuItem(item).click();
+    }
+
+    async hasDetailMenuItem(item: string) {
+        await expect(this.detailMenuItem(item)).toBeVisible();
+    }
+}
