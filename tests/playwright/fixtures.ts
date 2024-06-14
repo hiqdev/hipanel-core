5d5b415b77396b496fa6f865e471aaadef8cfd6b
Andrii Klochok <andreyklochok@gmail.com>
2024-06-14 12:40:15 +0300

    minor

--- tests/playwright/fixtures.ts
+++ tests/playwright/fixtures.ts
@@ -8,7 +8,6 @@ const testClients = {
   admin: { login: "hipanel_test_admin", password: "random" },
   manager: { login: "hipanel_test_manager", password: "random" },
   seller: { login: "hipanel_test_reseller", password: "random" },
-  osrc: { login: "osrc_testuser", password: "random" },
 };
 
 const doLogin = async (fileName: string, actor: string, browser: Browser) => {
@@ -23,7 +22,6 @@ export const test = base.extend<{
   adminPage: Page,
   managerPage: Page,
   sellerPage: Page,
-  osrcPage: Page,
 }>({
   storageState: async ({ browser }, use, testInfo: TestInfo) => {
     let actor;
@@ -34,7 +32,7 @@ export const test = base.extend<{
       }
     });
     if (!actor) {
-      throw new Error("Test role is not found, the role tag must be present in the test title, for example: @seller, @manager, @client, @admin, @osrc");
+      throw new Error("Test role is not found, the role tag must be present in the test title, for example: @seller, @manager, @client, @admin");
     }
     const fileName = path.join(process.cwd(), "tests/_data", `auth-storage-${actor}.json`);
     if (!fs.existsSync(fileName)) {
@@ -59,9 +57,6 @@ export const test = base.extend<{
     await use(page);
   },
   clientPage: async ({ page }, use) => {
-    await use(page);
-  },
-  osrcPage: async ({ page }, use) => {
     await use(page);
   },
 });
