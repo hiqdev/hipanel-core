1e200a097782942906b99b6c54e60e752c78f8e8
Andrii Klochok <andreyklochok@gmail.com>
2024-08-16 18:12:36 +0300

    HP-2057: TS

--- src/assets/MixAssetBundle.php
+++ src/assets/MixAssetBundle.php
@@ -1,9 +1,8 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace hipanel\assets;
 
+use yii\helpers\Json;
 use yii\web\AssetBundle;
 
 class MixAssetBundle extends AssetBundle
@@ -13,9 +12,13 @@ class MixAssetBundle extends AssetBundle
         parent::init();
         $path = $this->sourcePath . '/mix-manifest.json';
         if (is_file($path)) {
-            $content = json_decode(file_get_contents($path));
-            $file = ltrim(reset($content), '/');
-            $this->js[] = $file;
+            $files = Json::decode(file_get_contents($path));
+            if (empty($files)) {
+                return;
+            }
+            foreach ($files as $file) {
+                $this->js[] = ltrim($file, '/');
+            }
         }
     }
 }
