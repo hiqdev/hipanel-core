4e84e9900c5cfc7bf6169aad785caf06f198cab4
Andrii Klochok <andreyklochok@gmail.com>
2024-11-02 13:04:24 +0200

    HP-2146: if one visible field, do not try grid layout

--- src/widgets/views/bulk-operation.php
+++ src/widgets/views/bulk-operation.php
@@ -2,6 +2,7 @@
 
 use hipanel\helpers\Url;
 use hipanel\widgets\ArraySpoiler;
+use yii\base\Model;
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 
@@ -13,67 +14,79 @@ use yii\helpers\Html;
  * @var string $formatterField
  * @var string $submitButton
  * @var array $submitButtonOptions
- * @var \yii\base\Model $model
- * @var \yii\base\Model[] $models
+ * @var Model $model
+ * @var Model[] $models
  * @var string[] $hiddenInputs
  * @var string[] $visibleInputs
  * @var string[] $dropDownInputs
  */
 
-$form = ActiveForm::begin([
-    'id' => "bulk-{$scenario}-form",
+?>
+
+<?php $form = ActiveForm::begin([
+    'id' => "bulk-$scenario-form",
     'action' => Url::toRoute($scenario),
     'enableAjaxValidation' => false,
 ]);
 ?>
 
-    <?php if ($bodyWarning) : ?>
-        <div class="callout callout-warning">
-            <h4><?= $bodyWarning ?></h4>
-        </div>
-    <?php endif ?>
-
-    <div class="panel panel-default">
-        <div class="panel-heading"><?= $affectedObjects ?></div>
-        <div class="panel-body">
-            <?php if ($panelBody === null) : ?>
-                <?= ArraySpoiler::widget([
-                    'data' => array_map(fn ($model) => $model->{$formatterField}, $models),
-                    'visibleCount' => count($models),
-                    'delimiter' => ',&nbsp; ',
-                ]); ?>
-            <?php else : ?>
-                <?= $panelBody ?>
-            <?php endif ?>
-        </div>
+<?php if ($bodyWarning) : ?>
+    <div class="callout callout-warning">
+        <h4><?= $bodyWarning ?></h4>
     </div>
+<?php endif ?>
 
-    <?php foreach ($models as $item) : ?>
-        <?php foreach ($hiddenInputs as $hiddenInput) : ?>
-            <?= Html::activeHiddenInput($item, "[$item->id]{$hiddenInput}") ?>
-        <?php endforeach ?>
+<div class="panel panel-default">
+    <div class="panel-heading"><?= $affectedObjects ?></div>
+    <div class="panel-body">
+        <?php if ($panelBody === null) : ?>
+            <?= ArraySpoiler::widget([
+                'data' => array_map(fn($model) => $model->{$formatterField}, $models),
+                'visibleCount' => count($models),
+                'delimiter' => ',&nbsp; ',
+            ]) ?>
+        <?php else : ?>
+            <?= $panelBody ?>
+        <?php endif ?>
+    </div>
+</div>
+
+<?php foreach ($models as $item) : ?>
+    <?php foreach ($hiddenInputs as $hiddenInput) : ?>
+        <?= Html::activeHiddenInput($item, "[$item->id]$hiddenInput") ?>
     <?php endforeach ?>
+<?php endforeach ?>
 
-    <div class="row">
-        <?php foreach ($dropDownInputs as $dropDownName => $dropDownValues) : ?>
-            <div class="col-sm-6">
-                <?= $form->field($model, $dropDownName)->dropDownList($dropDownValues, [
-                    'id' => "{$scenario}-{$dropDownName}",
-                    'name' => $dropDownName,
-                ]) ?>
-            </div>
-        <?php endforeach ?>
+<div class="row">
+    <?php foreach ($dropDownInputs as $dropDownName => $dropDownValues) : ?>
+        <div class="col-sm-6">
+            <?= $form->field($model, $dropDownName)->dropDownList($dropDownValues, [
+                'id' => "$scenario-$dropDownName",
+                'name' => $dropDownName,
+            ]) ?>
+        </div>
+    <?php endforeach ?>
+    <?php if (count($visibleInputs) > 1) : ?>
         <?php foreach ($visibleInputs as $visibleInput) : ?>
             <div class="col-sm-6">
                 <?= $form->field($model, $visibleInput)->textInput([
-                    'id' => "{$scenario}-comment",
+                    'id' => "$scenario-comment",
                     'name' => $visibleInput,
                 ]) ?>
             </div>
         <?php endforeach ?>
-    </div>
+    <?php else : ?>
+        <div class="col-sm-12">
+            <?= $form->field($model, reset($visibleInputs))->textarea([
+                'id' => "$scenario-comment",
+                'name' => reset($visibleInputs),
+            ]) ?>
+        </div>
+    <?php endif ?>
+</div>
 
-    <div class="modal-footer">
-        <?= Html::submitButton($submitButton, $submitButtonOptions) ?>
-    </div>
+<div class="modal-footer">
+    <?= Html::submitButton($submitButton, $submitButtonOptions) ?>
+</div>
+
 <?php ActiveForm::end() ?>
