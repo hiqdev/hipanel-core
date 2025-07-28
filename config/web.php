ddceea3459e84bb88aec9d13965ca313824ea302
Yurii Myronchuk <bladeroot@hiqdev.com>
2025-07-28 13:38:15 +0000

    Cyclic call getSession

--- config/web.php
+++ config/web.php
@@ -148,9 +148,6 @@ return [
         ],
         'singletons' => [
             \hipanel\widgets\filePreview\FilePreviewFactoryInterface::class => \hipanel\widgets\filePreview\FilePreviewFactory::class,
-            \yii\web\Session::class => function () {
-                return Yii::$app->getSession();
-            },
             \yii\web\User::class => function () {
                 return Yii::$app->getUser();
             },
